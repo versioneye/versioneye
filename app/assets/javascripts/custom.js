69ed54f738e460f402b69c2ae453d449473724f8
reiz <robert.reiz@gmx.com>
2011-12-08 11:24:45 +0100

    adding ajax

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	
 });
 
-function follow_link(product_name){
+function follow_link(product_name, product_key){
 	$('#dialog_follow').dialog(
 		{
 		resizable: false, 
@@ -12,6 +12,12 @@ function follow_link(product_name){
 		minWidth: 600, 
 		create: function(event, ui) { 
 			document.getElementById('product_to_follow').innerHTML = product_name;
+			document.getElementById('product_hidden').value = product_key;
 			}
 		});
+}
+
+function validateEmail(elementValue){  
+	var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+	return emailPattern.test(elementValue);  
 }
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,10 @@ class ProductsController < ApplicationController
       flash.now[:error] = "Search field is empty. Please type in a search term."
     elsif @query.length == 1
       flash.now[:error] = "Search term is to short. Please type in at least 2 characters."
+    elsif @query.include?("%")
+      flash.now[:error] = "the character % is not allowed"
     else  
-      @products = Product.find_by_name(@query).paginate(:page => params[:page], :per_page => 10)
+      @products = Product.find_by_name(@query).paginate(:page => params[:page], :per_page => 30)
       if @products.nil? || @products.length == 0
         flash.now[:info] = "Sorry. No Results found."
       end
@@ -23,6 +25,14 @@ class ProductsController < ApplicationController
     key.gsub!("--","/")
     key.gsub!("~",".")
     @product = Product.find_by_key( key )
+  end
+  
+  def follow
+    
+    respond_to do |format|
+      format.html { redirect_to @prdouct }
+      format.js
+    end
   end
 
 end
