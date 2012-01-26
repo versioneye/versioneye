ab0222a3552b194f4568312cd3a976e76291014d
reiz <robert.reiz@gmx.com>
2012-01-26 18:04:12 +0100

    bugfix for setting locale

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -140,11 +140,13 @@ class UsersController < ApplicationController
     
     def set_locale
       locale = params[:locale]
-      if (locale.nil? || locale.empty?)
-        I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-      else
+      if (!locale.nil? && !locale.empty?)
         I18n.locale = locale
+      else  
+        I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
       end
+    rescue
+      nil
     end
 
 end
