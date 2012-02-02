7d55ba27800732e313aece06b145764b220f3bd4
reiz <robert.reiz@gmx.com>
2012-02-02 19:09:19 +0100

    adding twitter share button

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,4 +4,8 @@ module ProductsHelper
     "/product/#{product.to_param}/version/#{product.version_to_url_param}/#{product.get_decimal_version_uid}"
   end
   
+  def product_version_path_short(product)
+    "/product/#{product.to_param}/version/#{product.version_to_url_param}/1"
+  end
+  
 end
