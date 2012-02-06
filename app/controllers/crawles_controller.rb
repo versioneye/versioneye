6fb40fa2add3769dff5edd01ae4ee4deaad2798f
reiz <robert.reiz@gmx.com>
2012-02-06 13:04:26 +0100

    bugix for big data

--- app/controllers/crawles_controller.rb
+++ app/controllers/crawles_controller.rb
@@ -2,7 +2,7 @@ class CrawlesController < ApplicationController
 
   def index
     if signed_in_admin?
-      @crawles = Crawle.all().desc(:created_at).limit(20)    
+      @crawles = Crawle.all().desc(:created_at).limit(40)    
     end
   end
   
