86cf1c12ecdcc6221d5bb746ef8538c441dc1533
reiz <robert.reiz@gmx.com>
2012-02-06 13:03:43 +0100

    bugfix for big data by crawles

--- app/controllers/crawles_controller.rb
+++ app/controllers/crawles_controller.rb
@@ -2,7 +2,7 @@ class CrawlesController < ApplicationController
 
   def index
     if signed_in_admin?
-      @crawles = Crawle.all().desc(:created_at).limit(100)    
+      @crawles = Crawle.all().desc(:created_at).limit(20)    
     end
   end
   
