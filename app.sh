f756bcdf6dc2b503d45d7c332b165578b088a205
reiz <robert.reiz@gmx.com>
2012-02-29 16:13:19 -0800

    small changes for deploy script

--- app.sh
+++ app.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# josh 11-23-2004
-# shell script to ...
 
-#set the full path to the programs we need to use
 UNICORN=/var/lib/gems/1.9.1/bin/unicorn_rails
 KILLALL=/usr/bin/killall
 
@@ -20,23 +17,7 @@ case "$1" in
                 rake assets:precompile
                 unicorn_rails -p8080 -D
                 ;;
-        start)
-                #run ntop for web monitoring
-                echo "Starting app1..."
-                $UNICORN -p8080 -D
-                ;;
-        stop)
-                #kill ntop
-                echo "Stopping nXtop..."
-                $KILLALL UNICORN
-                ;;
-        restart)
-                $0 stop
-                $0 start
-                ;;
-        status)
-                ;;
         *)
-                echo "Usage: $0 {start|stop|restart|status}"
+                echo "Usage: $0 {deploy}"
                 ;;
 esac
