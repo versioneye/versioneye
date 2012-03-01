4eda6421362307a1f1f9da6ed0d3fc357492e79c
reiz <robert.reiz@gmx.com>
2012-02-29 16:09:05 -0800

    bugfix for duration in crawle

--- app/models/crawle.rb
+++ app/models/crawle.rb
@@ -1,10 +1,13 @@
 class Crawle
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::MultiParameterAttributes
   field :crawler_name, type: String
   field :crawler_version, type: String
   field :repository_src, type: String
   field :exec_group, type: String
+  field :duration, type: DateTime
+  
   embeds_many :errors
   
   def crawler_name_and_version
