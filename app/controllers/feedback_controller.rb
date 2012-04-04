cb11a6501477c4a8a341d64d781e06cad4bd1d81
reiz <robert.reiz@gmx.com>
2012-04-03 17:02:30 -0700

    adding math to feedback form for not signed in users

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -4,11 +4,16 @@ class FeedbackController < ApplicationController
     name = params[:fb_fullname]
     email = params[:fb_email]
     feedback = params[:feedback]
-    p "#{name}, #{email}, #{feedback}"
+    math = params[:fb_math]
+    
+    if !signed_in?
+      
+    end
+    
     if !name.nil? && !name.empty? && !email.nil? && !email.empty? && !feedback.nil? && !feedback.empty?
-      p "feedback email " 
-      #FeedbackMailer.delay.feedback_email(name, email, feedback)
-      FeedbackMailer.feedback_email(name, email, feedback).deliver
+      if signed_in? || math.eql?("4")
+        FeedbackMailer.feedback_email(name, email, feedback).deliver
+      end
     end    
     respond_to do |format|
       format.js
