237e6801c3896a3baf6769800d0fd9b76acba46d
reiz <robert.reiz@gmx.com>
2012-04-10 10:12:50 -0700

    new icon for login with github

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -4,23 +4,10 @@ class GithubController < ApplicationController
 
   def callback
     code = params['code']
-
-    @user = User.new
-    domain = 'https://github.com/'
-    uri = 'login/oauth/access_token'
-    query = 'client_id=50fb47103b8a3f03b2cd&'
-    query += 'client_secret=621051dd2033682449f62a5452a0e54b6c432907&'
-    query += 'code=' + code
-    link = "#{domain}#{uri}?#{query}"
-
-    doc = Nokogiri::HTML( open( URI.encode(link) ) )
-    p_element = doc.xpath('//body/p')
-    p_string = p_element.text
-    pips = p_string.split("&")
-    token = pips[0].split("=")[1]
-    type  = pips[1].split("=")[1]
-    
+    token = get_token( code )
+    p "token: #{token}"
     json_user = get_json_user( token )
+    p "json_user: #{json_user}"
     user = get_user_for_token( json_user, token )
     if !user.nil?
       sign_in user
@@ -28,6 +15,7 @@ class GithubController < ApplicationController
       return 
     else
       cookies.permanent.signed[:github_token] = token
+      @user = User.new
       render "new"
     end
   end
@@ -60,12 +48,14 @@ class GithubController < ApplicationController
         return
       end
       json_user = get_json_user( token )
+      p "json_user: #{json_user}"
       user = User.new
       user.update_from_github_json(json_user, token)
       user.email = @email
       user.terms = true
       user.datenerhebung = true
       user.create_verification
+      p "user before save!: #{user}"
       if user.save
         user.send_verification_email
         User.new_user_email(user)
@@ -78,8 +68,24 @@ class GithubController < ApplicationController
       end
     end
   end
+  
+  def get_token( code )
+    domain = 'https://github.com/'
+    uri = 'login/oauth/access_token'
+    query = 'client_id=50fb47103b8a3f03b2cd&'
+    query += 'client_secret=621051dd2033682449f62a5452a0e54b6c432907&'
+    query += 'code=' + code
+    link = "#{domain}#{uri}?#{query}"
 
-  private
+    doc = Nokogiri::HTML( open( URI.encode(link) ) )
+    p_element = doc.xpath('//body/p')
+    p_string = p_element.text
+    pips = p_string.split("&")
+    token = pips[0].split("=")[1]
+    token
+  end
+
+
 
     def get_json_user( token )
       json_user = JSON.parse HTTParty.get('https://api.github.com/user?access_token=' + URI.escape(token) ).response.body
