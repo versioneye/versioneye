0c7d402510214a5fb33d7ad7e70ef198b7b5a4f1
reiz <robert.reiz@gmx.com>
2012-04-17 12:23:18 -0700

    add authenticate filter to news controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,7 @@
 class NewsController < ApplicationController
   
+  before_filter :authenticate
+
   def index
   	 prod_keys = Versioncomment.get_prod_keys_for_user(current_user.id)
   	 @comments = Versioncomment.find_by_prod_keys(prod_keys).paginate(:page => params[:page]) 
