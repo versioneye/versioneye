e9abea42de5b7849b56084d5a107c9f7fc735b2b
reiz <robert.reiz@gmx.com>
2012-04-22 17:44:22 -0700

    new app.sh

--- app.sh
+++ app.sh
@@ -4,13 +4,10 @@ case "$1" in
         update)
                 git checkout Gemfile
                 git checkout Gemfile.lock
-                git checkout config/database.yml
                 git checkout config/mongoid.yml
                 git pull
-                cp ../database.yml config/database.yml
                 cp ../mongoid.yml config/mongoid.yml
                 bundle install
-                rake assets:precompile
                 ;;
         *)
                 echo "Usage: $0 {update}"
