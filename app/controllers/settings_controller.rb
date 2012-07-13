8c84e7e08e0a0fb1e7af799d449e2c0d5ce5f1a1
reiz <robert.reiz.81@gmail.com>
2012-07-13 11:57:00 +0200

    remove connect links

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -34,6 +34,25 @@ class SettingsController < ApplicationController
     @user = current_user
   end
 
+  def disconnect
+    user = current_user
+    service = params[:service]
+    if service && service.eql?("twitter")
+      user.twitter_id = nil
+      user.twitter_token = nil
+      user.twitter_secret = nil
+    elsif service && service.eql?("facebook")
+      user.fb_id = nil
+      user.fb_token = nil
+    elsif service && service.eql?("github")
+      user.github_id = nil
+      user.github_token = nil 
+      user.github_scope = nil
+    end
+    user.save
+    redirect_to settings_connect_path
+  end
+
   def updatenames
     fullname = params[:fullname]
     new_username = params[:new_username] 
