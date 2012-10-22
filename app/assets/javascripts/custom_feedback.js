bc15a2da8da7e3bda46a1375ccf2f35ede02b7ed
TimGLuz <timgluz@gmail.com>
2012-10-22 13:11:58 +0200

    Bootstrappified feedback form

--- app/assets/javascripts/custom_feedback.js
+++ app/assets/javascripts/custom_feedback.js
@@ -1,4 +1,14 @@
 
+function text_counter(max_size){
+	var feedback = jQuery('[name="feedback"]').val();
+	jQuery('.feedback-help').text = max_size - feedback.length;
+}
+
+
+function validateEmail(email){
+	return true;
+}
+
 
 function load_dialog_feedback(){
 	value_a = Math.floor((Math.random()*10)+1);
@@ -9,27 +19,29 @@ function load_dialog_feedback(){
 	a_element.value = value_a;
 	b_element.value = value_b;
 	fb_math_label.innerHTML = value_a + " + " + value_b + " = ";
-	jQuery('#dialog_feedback').dialog(
-		{
-		resizable: false, 
-		modal: true, 
-		draggable: false, 
-		minWidth: 700, 
-		});	
+	jQuery('#dialog_feedback').modal({keyboard : true});	
 }
 
 function validateFeedbackForm(){	
 	var result = true
-	var fullname = document.getElementById('fb_fullname').value;	
+	var fullname = jQuery('[name="fb_fullname"]').val();	
 	if (fullname == "" || fullname == null){
 		alert('Please type in your Name.')
 		result = false
 	}
-	var email = document.getElementById('fb_email').value;	
-	result = validateEmail(email)
-	var feedback = document.getElementById('feedback').value;	
+	var email = jQuery('[name="fb_email"]').val();	
+
+	result = validateEmail(email);
+	var feedback = jQuery('[name="feedback"]').val();	
 	if (feedback == "" || feedback == null){
 		alert('Please type in your Feedback.')
 		result = false
 	}
+
+
+	if (result == true){
+		//hide modal
+		jQuery('#dialog_feedback').modal('hide');
+	}
+	return result;
 }
