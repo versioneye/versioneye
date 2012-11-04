6642e8050afe8f3fcd6580272cc53442955f5885
reiz <robert.reiz.81@gmail.com>
2012-11-04 01:27:17 +0100

    correct the links for ruby projects

--- app/migrations/product_migration.rb
+++ app/migrations/product_migration.rb
@@ -88,6 +88,18 @@ class ProductMigration
 			product.save
 		end
 	end
+  
+  def self.improve_ruby_links()
+    Product.where(language: "Ruby").each do |product|
+      Versionlink.all(conditions: { prod_key: product.prod_key }).each do |link| 
+        if !link.version_id.nil?
+          p "improve link #{product.prod_key} - #{link.link} - #{link.version_id}"
+          link.version_id = nil 
+          link.save
+        end
+      end
+    end
+  end
 
 	def self.check_emtpy_release_dates(lang)
 		Product.where(language: lang).each do |product|
