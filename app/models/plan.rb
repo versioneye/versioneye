51a9616245286544c4d083f365e765e9fce1e767
reiz <robert.reiz.81@gmail.com>
2012-11-15 19:55:13 -0800

    updating plans

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -10,39 +10,36 @@ class Plan
 
   def self.create_default_plans
     free = Plan.new
-    free.name_id = "01_free"
+    free.name_id = "02_free"
     free.name = "Free"
     free.price = "0"
     free.private_projects = 0
     free.save
 
   	plan = Plan.new
-  	plan.name_id = "01_micro"
-  	plan.name = "Micro"
-  	plan.price = "7"
-  	plan.private_projects = 1
+  	plan.name_id = "02_personal"
+  	plan.name = "Personal"
+  	plan.price = "3"
+  	plan.private_projects = 5
   	plan.save
 
   	plan2 = Plan.new
-  	plan2.name_id = "01_small"
-  	plan2.name = "Small"
-  	plan2.price = "12"
-  	plan2.private_projects = 2
+  	plan2.name_id = "02_business_small"
+  	plan2.name = "Business - Small"
+  	plan2.price = "7"
+  	plan2.private_projects = 10
   	plan2.save
 
   	plan3 = Plan.new
-  	plan3.name_id = "01_medium"
-  	plan3.name = "Medium"
-  	plan3.price = "20"
-  	plan3.private_projects = 4
+  	plan3.name_id = "02_business_normal"
+  	plan3.name = "Business - Normal"
+  	plan3.price = "25"
+  	plan3.private_projects = 50
   	plan3.save
+  end
 
-  	plan4 = Plan.new
-  	plan4.name_id = "01_large"
-  	plan4.name = "Large"
-  	plan4.price = "47"
-  	plan4.private_projects = 10
-  	plan4.save
+  def self.current_plans
+    Plan.where(name_id: /^02/)
   end
   
 end
