7fdf22a8730ed3dc2f206dc715d464106363c2f0
Timo Sulg <timgluz@gmail.com>
2012-12-25 22:30:44 +0100

    ticket.53 - socialbuttons css update; fixes for feedback form: updated math_captcha and labels

--- app/assets/javascripts/custom_lp.js
+++ app/assets/javascripts/custom_lp.js
@@ -6,9 +6,26 @@ jQuery(document).ready(function() {
 	});
 
 
+  if (jQuery('.fb_math').length){
+     render_captcha_math()      
+  }
+
 	
 });
+
 
 function load_video(){
   jQuery('#dialog_video').modal({keyboard : true});
 }
+
+
+function render_captcha_math(){
+	value_a = Math.floor((Math.random()*10)+1);
+	value_b = Math.floor((Math.random()*10)+1);
+	jQuery("[name='value_a']").val(value_a);
+	jQuery("[name='value_b']").val(value_b);
+	fb_math_label = jQuery(".fb_math");
+	fb_math_label.html(value_a + " + " + value_b + " = ");
+}
+
+
