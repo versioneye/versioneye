28e172bb318e882dd453df87f67a7185fcd5e4b4
Timo Sulg <timgluz@gmail.com>
2012-12-26 14:52:49 +0100

    ticket.58 - added approved_url_email

---
+++ app/mailers/submitted_url_mailer.rb
@@ -0,0 +1,11 @@
+class SubmittedUrlMailer < ActionMailer::Base
+  default from: "\"VersionEye\" <notify@versioneye.com>"
+
+  def approved_url_email(user_email, submitted_url)
+      @submitted_url = submitted_url
+      mail(
+          :to       => user_email,
+          :subject  => "Your URL is accepted.", 
+          :tag      => "notice")
+  end
+end
