1f9f7465198c7dc046f0730b86e2685166a981e3
reiz <robert.reiz.81@gmail.com>
2013-02-06 01:09:47 +0100

    siplifying the feedback form

--- app/assets/javascripts/custom_feedback.js
+++ app/assets/javascripts/custom_feedback.js
@@ -16,14 +16,6 @@ function textCounter(field, cntfield, maxlimit) {
 }
 
 function load_dialog_feedback(){
-	value_a = Math.floor((Math.random()*10)+1);
-	value_b = Math.floor((Math.random()*10)+1);
-	a_element = document.getElementById("value_a");
-	b_element = document.getElementById("value_b");
-	fb_math_label = document.getElementById("fb_math");
-	a_element.value = value_a;
-	b_element.value = value_b;
-	fb_math_label.innerHTML = value_a + " + " + value_b + " = ";
 	jQuery('#dialog_feedback').modal({keyboard : true});
 }
 
--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -4,21 +4,8 @@ class FeedbackController < ApplicationController
     name = params[:fb_fullname]
     email = params[:fb_email]
     feedback = params[:feedback]
-    math = params[:fb_math]
-    value_a = params[:value_a]
-    value_b = params[:value_b]
-
     if name && !name.empty? && email && !email.empty? && feedback && !feedback.empty?
-      @correct_math = false
-      if math && value_b && value_a
-        correct_result = value_a.to_i + value_b.to_i
-        if correct_result == math.to_i 
-          @correct_math = true 
-        end
-      end
-      if @correct_math
-        FeedbackMailer.feedback_email(name, email, feedback).deliver
-      end
+      FeedbackMailer.feedback_email(name, email, feedback).deliver
     end    
     respond_to do |format|
       format.js
