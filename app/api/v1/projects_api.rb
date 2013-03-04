5d2570dc99e080ded4d50e1900c8929fc8609dcc
reiz <robert.reiz.81@gmail.com>
2013-03-04 12:08:51 -0800

    bugfix for update project in api.

--- app/api/v1/projects_api.rb
+++ app/api/v1/projects_api.rb
@@ -83,7 +83,7 @@ module VersionEye
       post '/:project_key' do
         authorized?
 
-        @project = fetch_project_by_key_and_user(project_key, current_user)
+        @project = fetch_project_by_key_and_user( params[:project_key], current_user )
         if @project.nil?
           error! "Project `#{params[:project_key]}` dont exists", 400
         end
