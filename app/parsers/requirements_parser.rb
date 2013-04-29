c9658d5d661b4fd49c45a74ce3ddb4199c3e4c81
Timo Sulg <timgluz@gmail.com>
2013-04-29 19:09:23 +0300

    Fix for python_setup_parser; Added version period for parser;

--- app/parsers/requirements_parser.rb
+++ app/parsers/requirements_parser.rb
@@ -79,6 +79,62 @@ class RequirementsParser < CommonParser
     
     if product.nil? 
       dependency.version_requested = version
+ 
+    elsif version.match(/,/)
+      # Version Ranges
+      version_splitted = version.split(",")
+      prod = Product.new 
+      prod.versions = product.versions 
+      version_splitted.each do |verso|
+        verso.gsub!(" ", "")
+        if verso.match(/^>=/)
+          verso.gsub!(">=", "")  
+          new_range = prod.greater_than_or_equal( verso, true )
+          prod.versions = new_range
+        elsif verso.match(/^>/)
+          verso.gsub!(">", "")
+          new_range = prod.greater_than( verso, true )
+          prod.versions = new_range
+        elsif verso.match(/^<=/)
+          verso.gsub!("<=", "")
+          new_range = prod.smaller_than_or_equal( verso, true )
+          prod.versions = new_range
+        elsif verso.match(/^</)
+          verso.gsub!("<", "")
+          new_range = prod.smaller_than( verso, true )
+          prod.versions = new_range
+        elsif verso.match(/^!=/)
+          verso.gsub!("!=", "")
+          new_range = prod.newest_but_not(verso, true)
+          prod.versions = new_range
+        end
+      end
+      highest_version = Product.newest_version_from( prod.versions )
+      if highest_version
+        dependency.version_requested = highest_version.version
+      else 
+        dependency.version_requested = version
+      end
+      dependency.comperator = "="
+    
+    
+    elsif version.match(/.\*$/)
+      # WildCards. 1.0.* => 1.0.0 | 1.0.2 | 1.0.20
+      ver = version.gsub("*", "")
+      ver = ver.gsub(" ", "")
+      highest_version = product.newest_version_from_wildcard( ver, dependency.stability )
+      if highest_version
+        dependency.version_requested = highest_version
+      else 
+        dependency.version_requested = version
+      end
+      dependency.comperator = "="
+
+    elsif version.empty? || version.match(/^\*$/)
+      # This case is not allowed. But we handle it anyway. Because we are fucking awesome!
+      dependency.version_requested = product.newest_version_number( dependency.stability )
+      dependency.version_label = "*"
+      dependency.comperator = "="
     
     elsif version.match(/^==/)
       # Equals
