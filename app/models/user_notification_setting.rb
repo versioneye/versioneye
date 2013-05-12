ea0b9e515b82167828494866f239052447c734c8
reiz <robert.reiz.81@gmail.com>
2013-05-12 21:33:54 +0200

    Refactor UserNotificationSetting and add Tests for it.

--- app/models/user_notification_setting.rb
+++ app/models/user_notification_setting.rb
@@ -3,34 +3,42 @@ class UserNotificationSetting
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :newsletter_news, type: Boolean, default: true      # getting general news from VersionEye
-  field :newsletter_features, type: Boolean, default: true  # getting new features newsletter from VersionEye
+  # Receiving general news from VersionEye. For example about Investments. 
+  field :newsletter_news,     type: Boolean, default: true  
+  
+  # Receiving the newsletter about new features
+  field :newsletter_features, type: Boolean, default: true  
 
   belongs_to :user 
 
-  def self.get_by_user_id(id)
-  	UserNotificationSetting.where(user_id: id)[0]
-  end
-
-  def self.send_newsletters_features
+  def self.send_newsletter_features
+    count = 0 
     users = User.all()
     users.each do |user|
-      notification_setting = user.user_notification_setting
-      if user.deleted != true && ( notification_setting.nil? || notification_setting.newsletter_features )
+      next if user.deleted
+      notification_setting = self.fetch_or_create_notification_setting( user )
+      if notification_setting.newsletter_features
         UserNotificationSetting.send_newsletter_new_features_for_user( user )
+        count += 1
       end
     end
+    count 
+  end
+
+  def self.fetch_or_create_notification_setting user 
+    if user.user_notification_setting.nil? 
+      user.user_notification_setting = UserNotificationSetting.new 
+      user.save   
+    end
+    user.user_notification_setting
   end
 
   def self.send_newsletter_new_features_for_user( user )
-    p "send newsletter about new features to user #{user.fullname} - start"
+    Rails.logger.info "Send new feature newsletter to #{user.fullname}"
     NewsletterMailer.newsletter_new_features_email(user).deliver
   rescue => e
-    p "An error occured. E-Mail inactive: #{user.fullname} - #{user.email}"
-    p "ERROR #{e}"
-    e.backtrace.each do |message| 
-      p " - #{message}"
-    end
+    Rails.logger.error " -- An error occured. E-Mail inactive: #{user.fullname} - #{user.email}"
+    Rails.logger.error e
   end
 
 end
