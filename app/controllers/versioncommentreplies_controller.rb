9b876c21cc021b20695a3f6edd2851bbc2a5fdb9
reiz <robert.reiz.81@gmail.com>
2013-05-12 20:37:32 +0200

    Refactor Versioncomment.product

--- app/controllers/versioncommentreplies_controller.rb
+++ app/controllers/versioncommentreplies_controller.rb
@@ -41,8 +41,7 @@ class VersioncommentrepliesController < ApplicationController
     def send_comment_reply_mails(user, comment)
       comment_user = comment.user
       if !comment_user.username.eql?(user.username)
-        product = comment.get_product
-        VersioncommentreplyMailer.versioncomment_reply_email(comment_user, user, comment, product).deliver
+        VersioncommentreplyMailer.versioncomment_reply_email(comment_user, user, comment).deliver
       end
     end
   
