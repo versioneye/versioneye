670a138a5d027e104ba1eb0d4dbf17756aa9134b
reiz <robert.reiz.81@gmail.com>
2013-05-14 21:38:25 +0200

    Add new newsletter

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,13 +1,13 @@
 class NewsletterMailer < ActionMailer::Base
   default from: "\"VersionEye\" <notify@versioneye.com>"
-  
+
   def newsletter_new_features_email(user)
     @user = user
     mail(
-      :to => @user.email, 
-      :subject => "VersionEye News",
+      :to => @user.email,
+      :subject => "Hasso Plattner Ventures invests 100K EUR into VersionEye",
       :tag => "newsletter"
       )
   end
-  
+
 end
