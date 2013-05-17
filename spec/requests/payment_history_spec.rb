963bdc78897202144ea48266d6091d238e01d3b4
Timo Sulg <timgluz@gmail.com>
2013-05-17 21:47:44 +0300

    spec without JS is running; Rspec-capybara dont call ajax;

--- spec/requests/payment_history_spec.rb
+++ spec/requests/payment_history_spec.rb
@@ -2,35 +2,36 @@ require 'spec_helper'
 
 describe "Payment History" do
   before :each do
-    @test_user = UserFactory.default 
-  end
-
-  after :each do
-    @test_user.delete
-  end
-
-  it "shows correct message when there's no history" do
-   
+    @test_user = UserFactory.default
+    @test_user.save
+    host!('127.0.0.1:3000')
+    
     post "/sessions", 
       {:session => {email: @test_user.email, password: @test_user.password}}, 
       "HTTPS" => "on"
     assert_response 302
     response.should redirect_to("/user/projects")
 
-    get "/settings/payments" 
-    p response.body
-    assert_response :success
-    have_css "#payment_history", text: "You dont have any Payment history"
-    Launchy.open("127.0.0.1:3000/settings/payments")
+
   end
 
-  describe "when user id is missing or is uncorrect" do
+  after :each do
+    @test_user.delete
+  end
+
+  describe "when stripe id is missing or is uncorrect", :js => true do
     before :each do
-      recorded_response = "curl https://api.stripe.com/v1/invoices/ -u sk_test_mkGsLqEW6SLnZa487HYfJVLf:"
       FakeWeb.register_uri(
         :get, 
         %r|https://api\.stripe\.com/v1/invoices/*|,
-        body: recorded_response 
+        body: <<-JSON
+          {
+            "object": "list",
+            "count": 0,
+            "url": "/v1/invoices",
+            "data": []
+          }
+        JSON
       )
     end
 
@@ -38,6 +39,38 @@ describe "Payment History" do
       FakeWeb.clean_registry
     end
 
+    it "shows correct message when there's no history" do
+       
+      get "/settings/payments", nil, "HTTPS" => "on" 
+      assert_response :success
+      have_css "#payment_history", text: "You dont have any Payment history"
+    end
+  end
+
+  describe "use correct stripe id", :js => true  do
+    before :each do
+      @test_user.stripe_customer_id = "cus_1m9PBsr4Xhumds"
+      @test_user.save
+      url = "https://api.stripe.com/v1/invoices?customer=%s -u %s:" % [@test_user.stripe_customer_id, Stripe.api_key]
+
+      recorded_response = `curl -is #{url}`      
+      FakeWeb.register_uri(
+        :get, 
+        %r|https://api\.stripe\.com/v1/invoices/*|,
+        response: recorded_response 
+      )
+   end
+
+    it "shows correct list of invoices for current user" do
+      #visit settings_payments_path;
+      get settings_payments_path, nil, "HTTPS" => "on"
+      assert_response :success
+      have_css "#payment_history"
+      
+      should_not have_text('Iniatilizing frontend app..')
+      save_and_open_page
+      response.should have_selector('table tr', :count => 2)
+    end
   end
 
 end
