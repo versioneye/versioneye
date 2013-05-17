83446824a3161236abe06f791e801438786955bc
reiz <robert.reiz.81@gmail.com>
2013-05-17 09:49:05 +0200

    small refactoring for GitHub repo switches

--- app/services_ext/github.rb
+++ app/services_ext/github.rb
@@ -12,22 +12,22 @@ class Github
   def self.oauth_scopes( token )
     resp = HTTParty.get("https://api.github.com/user?access_token=#{token}", :headers => {"User-Agent" => A_USER_AGENT } )
     resp.headers['x-oauth-scopes']
-  rescue => e 
+  rescue => e
     "no_scope"
   end
 
- 
+
   def self.user_repos(github_token)
     body = HTTParty.get("https://api.github.com/user/repos?access_token=#{github_token}", :headers => {"User-Agent" => A_USER_AGENT } ).response.body
     repos = JSON.parse( body )
     repos
   end
-  
+
   def self.user_repo_names( github_token )
     repos = self.user_repos(github_token)
     extract_repo_names( repos )
   end
-  
+
 
   def self.orga_repo_names( github_token )
     orga_names = self.orga_names github_token
@@ -36,22 +36,22 @@ class Github
 
   def self.repo_names_for_orgas( github_token, organisations )
     repo_names = Array.new
-    organisations.each do |orga| 
+    organisations.each do |orga|
       repo_names += self.repo_names_for_orga( github_token, orga )
     end
     repo_names
   end
 
   def self.repo_names_for_orga( github_token, organisation_name )
-    repo_names = Array.new 
+    repo_names = Array.new
     page = 1
-    loop do 
+    loop do
       body = HTTParty.get("https://api.github.com/orgs/#{organisation_name}/repos?access_token=#{github_token}&page=#{page}", :headers => {"User-Agent" => A_USER_AGENT} ).response.body
       repos = JSON.parse( body )
       break if ( repos.nil? || repos.empty? )
       repo_names += extract_repo_names( repos )
       page += 1
-    end 
+    end
     repo_names
   end
 
@@ -61,7 +61,7 @@ class Github
     message = get_message( organisations )
     names = Array.new
     if organisations.nil? || organisations.empty? || !message.nil?
-      return names 
+      return names
     end
     organisations.each do |organisation|
       names << organisation['login']
@@ -98,7 +98,7 @@ class Github
       result['url'] = file['url']
       result['name'] = name
       type = Project.type_by_filename( name )
-      if type 
+      if type
         result['type'] = type
         return result
       end
@@ -109,30 +109,30 @@ class Github
   def self.fetch_file( url, token )
     JSON.parse HTTParty.get( "#{url}?access_token=" + URI.escape(token), :headers => {"User-Agent" => A_USER_AGENT} ).response.body
   end
-  
+
   def self.supported_languages()
-    Set['java', 'ruby', 'python', 'node.js', 'php', 'javascript', 
+    Set['java', 'ruby', 'python', 'node.js', 'php', 'javascript',
         'coffeescript', 'clojure']
   end
 
-  private 
+  private
 
     def self.language_supported?(lang)
       return false if lang.nil?
-      lang.casecmp('Java') == 0 || 
-      lang.casecmp('Ruby') == 0 || 
-      lang.casecmp('Python') == 0 || 
-      lang.casecmp('Node.JS') == 0 || 
-      lang.casecmp("CoffeeScript") == 0 || 
-      lang.casecmp("JavaScript") == 0 || 
-      lang.casecmp("PHP") == 0 || 
-      lang.casecmp("Clojure") == 0 
+      lang.casecmp('Java') == 0 ||
+      lang.casecmp('Ruby') == 0 ||
+      lang.casecmp('Python') == 0 ||
+      lang.casecmp('Node.JS') == 0 ||
+      lang.casecmp("CoffeeScript") == 0 ||
+      lang.casecmp("JavaScript") == 0 ||
+      lang.casecmp("PHP") == 0 ||
+      lang.casecmp("Clojure") == 0
     end
 
     def self.get_message( repositories )
       repositories['message']
     rescue => e
-      # by default here should be no message or nil 
+      # by default here should be no message or nil
       # We expect that everything is ok and there is no error message
       nil
     end
@@ -141,7 +141,7 @@ class Github
       message = get_message( repos )
       repo_names = Array.new
       if repos.nil? || repos.empty? || !message.nil?
-        return repo_names 
+        return repo_names
       end
       repos.each do |repo|
         lang = repo['language']
