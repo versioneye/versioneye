c294d953a671e39e0ecc7012b900b278b7a8eb57
reiz <robert.reiz.81@gmail.com>
2013-05-26 22:33:42 +0200

    Advance the parsers for python

--- app/parsers/python_setup_parser.rb
+++ app/parsers/python_setup_parser.rb
@@ -3,51 +3,16 @@ class PythonSetupParser < RequirementsParser
   def parse(url)
     response = fetch_response(url)
     doc = response.body
-    return nil if doc.nil? or doc.empty? 
+    return nil if doc.nil? or doc.empty?
 
     requirements = parse_requirements(doc)
     extras = parse_extras(doc)
-    
-    project = Project.new 
+
+    project = Project.new
     project.dependencies = []
 
     requirements.each do |requirement|
-      requirement = requirement.strip
-      next if requirement.nil? or requirement.empty?
-
-      comparator = get_splitter requirement
-      package, version = requirement.split(comparator)
-    
-
-      key = "pip/#{package}"
-      product = Product.find_by_key(key)
-      if product.nil? 
-        product = Product.find_by_key_case_insensitiv(key)
-      end
-      
-      if product.nil?
-        project.unknown_number + 1 
-      else 
-        key = product.prod_key
-      end
-
-      if version.nil? && !product.nil? 
-        version = product.version
-      end
-
-      dependency = Projectdependency.new name: package.strip,
-                                         prod_key: key,
-                                         version_label: "#{version}".strip,
-                                         comperator: comparator,
-                                         scope: "compile"
-      
-      parse_requested_version("#{comparator}#{version}", dependency, product)
-
-      if dependency.outdated?
-        project.out_number = project.out_number + 1
-      end
-  
-      project.dependencies << dependency
+      parse_line requirement, project
     end
 
     project.dep_number = project.dependencies.count
@@ -58,10 +23,46 @@ class PythonSetupParser < RequirementsParser
     project
   end
 
+
+  def parse_line requirement, project
+    requirement = requirement.strip
+    return false if requirement.nil? or requirement.empty?
+
+    comparator = extract_comparator requirement
+    package, version = requirement.split comparator
+
+    product = fetch_product "pip/#{package}"
+
+    if version.nil? && !product.nil?
+      version = product.version
+    end
+
+    dependency = Projectdependency.new name: package.strip,
+                                       version_label: "#{version}".strip,
+                                       comperator: comparator,
+                                       scope: "compile"
+
+    if product.nil?
+      project.unknown_number + 1
+    else
+      dependency.prod_key = product.prod_key
+    end
+
+    parse_requested_version("#{comparator}#{version}", dependency, product)
+
+    if dependency.outdated?
+      project.out_number = project.out_number + 1
+    end
+
+    project.dependencies << dependency
+  end
+
+
   def from_file()
     doc_file = File.new "test/files/setup.py"
     doc_file.read
   end
+
 
   def parse_requirements(doc)
     return nil if doc.nil? or doc.empty?
@@ -69,23 +70,25 @@ class PythonSetupParser < RequirementsParser
     req_text.split(/\'/).keep_if {|item| item.strip.length > 1}
   end
 
+
   def parse_extras(doc)
     return nil if doc.nil? or doc.empty?
-    extras_txt = slice_content doc, 'extras_require', '{', '}', true 
+    extras_txt = slice_content doc, 'extras_require', '{', '}', true
     extras_txt
   end
+
 
   def slice_content(doc, keyword, start_matcher, end_matcher, include_matchers = false)
     return nil if doc.nil? or doc.empty?
 
     content_pos = doc.index keyword
-    return nil if content_pos.nil? 
+    return nil if content_pos.nil?
 
     start_pos = doc.index start_matcher, content_pos
     end_pos =  doc.index end_matcher, content_pos
 
     block_length = (end_pos - start_pos) + 1
-    if include_matchers 
+    if include_matchers
       req_txt = doc.slice start_pos, block_length
     else
       req_txt = doc.slice(start_pos + 1, block_length - 2)
--- app/parsers/requirements_parser.rb
+++ app/parsers/requirements_parser.rb
@@ -1,92 +1,94 @@
 class RequirementsParser < CommonParser
-  
+
   # Parser for requirements.txt files from pip. Python.
   # http://www.pip-installer.org/en/latest/requirements.html#the-requirements-file-format
   # http://www.pip-installer.org/en/latest/#requirements-files
   #
   def parse( url )
     return nil if url.nil?
-    
+
     response = self.fetch_response(url)
     txt = response.body
-    return nil if txt.nil?
-    
+    return nil if txt.nil? || txt.empty?
+
     project = Project.new
     project.dependencies = Array.new
-    
+
     txt.each_line do |line|
-
-      next if !line.match(/^#/).nil?
-
-      splitter = get_splitter line
-      requirement = line.split(splitter)
-      
-      next if requirement.empty? || requirement.count != 2
-      
-      package = requirement[0]
-      
-      next if package.strip.empty? 
-      
-      dependency = Projectdependency.new
-      dependency.name = package
-      dependency.comperator = splitter
-      dependency.scope = "compile"
-      
-      version = requirement[1]
-      dependency.version_label = version.strip
-      
-      key = "pip/#{package}"
-      product = Product.find_by_key(key)
-      if product.nil? 
-        product = Product.find_by_key_case_insensitiv(key)
-      end
-      if product
-        dependency.prod_key = product.prod_key
-      else 
-        project.unknown_number = project.unknown_number + 1
-      end
-      
-      parse_requested_version("#{splitter}#{version}", dependency, product)
-      
-      if dependency.outdated?
-        project.out_number = project.out_number + 1
-      end
-      project.dependencies << dependency
+      parse_line line, project
     end
+
     project.dep_number = project.dependencies.count
     project.project_type = Project::A_TYPE_PIP
     project.language = Product::A_LANGUAGE_PYTHON
     project.url = url
-    project
-  rescue => e 
+    return project
+  rescue => e
     Rails.logger.error e.message
     Rails.logger.error e.backtrace.first
   end
 
-  # It is important that this method is not writing int the database! 
+
+  def parse_line( line, project )
+    line.gsub!(" ", "")
+    return false if !line.match(/^#/).nil? || line.empty?
+
+    comparator  = extract_comparator line
+    requirement = line.split(comparator)
+    package = requirement[0]
+
+    return false if package.nil? || package.strip.empty?
+    return false if package.match(/^http:\/\//)
+    return false if package.match(/^https:\/\//)
+    return false if package.match(/^--/)
+
+    dependency = init_dependency package, comparator
+
+    if requirement.count > 1
+      version = requirement[1]
+      dependency.version_label = version.strip
+    end
+
+    product = fetch_product "pip/#{package}"
+    if product
+      dependency.prod_key = product.prod_key
+    else
+      project.unknown_number = project.unknown_number + 1
+    end
+
+    parse_requested_version("#{comparator}#{version}", dependency, product)
+
+    if dependency.outdated?
+      project.out_number = project.out_number + 1
+    end
+    project.dependencies << dependency
+  end
+
+
+  # It is important that this method is not writing int the database!
   #
   def parse_requested_version(version, dependency, product)
     if (version.nil? || version.empty?)
       self.update_requested_with_current(dependency, product)
-      return 
+      return
     end
     version = version.strip
     version = version.gsub('"', '')
     version = version.gsub("'", "")
     dependency.version_label = String.new(version)
-    
-    if product.nil? 
+
+    if product.nil?
       dependency.version_requested = version
- 
+
     elsif version.match(/,/)
       # Version Ranges
       version_splitted = version.split(",")
-      prod = Product.new 
-      prod.versions = product.versions 
+      prod = Product.new
+      prod.versions = product.versions
       version_splitted.each do |verso|
         verso.gsub!(" ", "")
         if verso.match(/^>=/)
-          verso.gsub!(">=", "")  
+          verso.gsub!(">=", "")
           new_range = prod.greater_than_or_equal( verso, true )
           prod.versions = new_range
         elsif verso.match(/^>/)
@@ -110,12 +112,12 @@ class RequirementsParser < CommonParser
       highest_version = Product.newest_version_from( prod.versions )
       if highest_version
         dependency.version_requested = highest_version.version
-      else 
+      else
         dependency.version_requested = version
       end
       dependency.comperator = "="
-    
-    
+
+
     elsif version.match(/.\*$/)
       # WildCards. 1.0.* => 1.0.0 | 1.0.2 | 1.0.20
       ver = version.gsub("*", "")
@@ -123,7 +125,7 @@ class RequirementsParser < CommonParser
       highest_version = product.newest_version_from_wildcard( ver, dependency.stability )
       if highest_version
         dependency.version_requested = highest_version
-      else 
+      else
         dependency.version_requested = version
       end
       dependency.comperator = "="
@@ -133,14 +135,14 @@ class RequirementsParser < CommonParser
       dependency.version_requested = product.newest_version_number( dependency.stability )
       dependency.version_label = "*"
       dependency.comperator = "="
-    
+
     elsif version.match(/^==/)
       # Equals
       version.gsub!("==", "")
       version.gsub!(" ", "")
       dependency.version_requested = version
       dependency.comperator = "=="
-    
+
     elsif version.match(/^!=/)
       # Not equal to version
       version.gsub!("!=", "")
@@ -148,7 +150,7 @@ class RequirementsParser < CommonParser
       newest_version = product.newest_but_not(version)
       dependency.version_requested = newest_version
       dependency.comperator = "!="
-    
+
     elsif version.match(/^>=/)
       # Greater than or equal to
       version.gsub!(">=", "")
@@ -156,7 +158,7 @@ class RequirementsParser < CommonParser
       newest_version = product.greater_than_or_equal(version)
       dependency.version_requested = newest_version.version
       dependency.comperator = ">="
-    
+
     elsif version.match(/^>/)
       # Greater than version
       version.gsub!(">", "")
@@ -164,7 +166,7 @@ class RequirementsParser < CommonParser
       newest_version = product.greater_than(version)
       dependency.version_requested = newest_version.version
       dependency.comperator = ">"
-    
+
     elsif version.match(/^<=/)
       # Less than or equal to
       version.gsub!("<=", "")
@@ -172,7 +174,7 @@ class RequirementsParser < CommonParser
       newest_version = product.smaller_than_or_equal(version)
       dependency.version_requested = newest_version.version
       dependency.comperator = "<="
-    
+
     elsif version.match(/^\</)
       # Less than version
       version.gsub!("<", "")
@@ -180,26 +182,49 @@ class RequirementsParser < CommonParser
       newest_version = product.smaller_than(version)
       dependency.version_requested = newest_version.version
       dependency.comperator = "<"
-    
+
     else
       dependency.version_requested = version
       dependency.comperator = "=="
     end
-    
+
   end
-  
-  def get_splitter line
-    splitter = "=="
-    if !line.match(/>=/).nil?
-      splitter = ">="
-    elsif !line.match(/>/).nil?
-      splitter = ">"  
-    elsif !line.match(/<=/).nil?
-      splitter = "<="
-    elsif !line.match(/</).nil?
-      splitter = "<"
+
+
+  def init_dependency package, comparator
+    dependency = Projectdependency.new
+    dependency.name = package
+    dependency.comperator = comparator
+    dependency.scope = "compile"
+    dependency
+  end
+
+
+  def fetch_product key
+    product = Product.find_by_key(key)
+    if product.nil?
+      product = Product.find_by_key_case_insensitiv(key)
     end
-    splitter
+    product
   end
-  
+
+
+  def extract_comparator line
+    comparator = nil
+    if !line.match(/>=/).nil?
+      comparator = ">="
+    elsif !line.match(/>/).nil?
+      comparator = ">"
+    elsif !line.match(/<=/).nil?
+      comparator = "<="
+    elsif !line.match(/</).nil?
+      comparator = "<"
+    elsif !line.match(/!=/).nil?
+      comparator = "!="
+    elsif !line.match(/==/).nil?
+      comparator = "=="
+    end
+    comparator
+  end
+
 end
