e6162ff7de9566f8f3d25d23c8616a94ed5321e4
reiz <robert.reiz.81@gmail.com>
2013-06-05 11:31:28 +0200

    Improve the elastic search implementation for small data sets.

--- spec/elastics/product_elastic_spec.rb
+++ spec/elastics/product_elastic_spec.rb
@@ -9,14 +9,14 @@ end
 def get_index_count
   response = RestClient.get("http://localhost:9200/product_test/_count")
   JSON.parse(response)["count"]
-end 
+end
 
 describe ProductElastic do
 
   before :each do
     ProductElastic.clean_all #remove all previous indexes at elasticsearch
     ProductElastic.create_index_with_mappings
-    @prods = [    
+    @prods = [
       {:name => "club-mate",        :language => "Java", :group_id => "org.club.mate"},
       {:name => "club-mate-parent", :language => "Java", :group_id => "com.club.mate"},
       {:name => "club-mate-child",  :language => "Java", :group_id => "net.club.mate"},
@@ -29,32 +29,33 @@ describe ProductElastic do
       {:name => "superb_mate.jar",  :language => "mate", :group_id => "club.mate.org"}
     ]
     @products = Array.new
-    @prods.each do |prod| 
+    @prods.each do |prod|
       product = Product.new(prod)
       product.save
       ProductElastic.index product
       @products.push product
     end
+    sleep 3
   end
 
-  after :each do    
+  after :each do
     ProductElastic.clean_all
     Product.where().delete
   end
-  
+
   context "With no indexes: " do
-    
+
     it "does clean_all successfull" do
       begin
         ProductElastic.clean_all # clean all previous accidental data
         # if elasticsearch is empty, then it should return false
         ProductElastic.clean_all
         "true".should eql("false")
-      rescue 
+      rescue
         "true".should eql("true")
       end
     end
-    
+
     it "is nil because no index" do
       begin
         ProductElastic.search("random query")
@@ -63,54 +64,55 @@ describe ProductElastic do
         "true".should eql("true")
       end
     end
-  
+
   end
 
+
   context "Only one product in Index." do
-    
+
     it "adds one element to the index" do
       product = @products[0]
       result = ProductElastic.index product
       result.response.code.should eql(200)
-    end 
-    
+    end
+
     it "Finds the only element in the index by name" do
       ProductElastic.clean_all
       product = Product.new(:name => "rails")
       product.save
       ProductElastic.index product
-      sleep 2 # sleep for 2 seconds until the product gets indexed via REST. 
+      sleep 2 # sleep for 2 seconds until the product gets indexed via REST.
       results = ProductElastic.search "rails"
       results.count.should eql(1)
     end
 
   end
 
+
   context "Club-Mate in the house" do
-    
     it "Finds club-mate first!" do
-      sleep 7
       results = ProductElastic.search "club-mate"
       results.count.should eql(7)
       results[0].name.should eql("club-mate") #async adding&indexing gaves diff results
     end
-
   end
+
 
   context "- index only documents, which has flagged to reindex" do
     it "index_newest" do
       ProductElastic.clean_all
       @products.each do |product|
-        product.update_attribute(:reindex, true)            
+        product.update_attribute(:reindex, true)
       end
       ProductElastic.index_newest
-      get_index_count.should equal @products.count 
+      get_index_count.should equal @products.count
 
-      Product.where(reindex: true).count.should equal 0               
+      Product.where(reindex: true).count.should equal 0
     end
   end
 
-  context " - index all documents in `products` collection" do 
+
+  context " - index all documents in `products` collection" do
     it "index_all" do
       ProductElastic.clean_all
       ProductElastic.create_index_with_mappings
@@ -118,20 +120,21 @@ describe ProductElastic do
       add_local_products
       ProductElastic.index_all
       sleep 4
-      get_index_count.should eql(Product.count) 
+      get_index_count.should eql(Product.count)
     end
   end
 
+
   context "- tests search functionalities " do
-    
+
     it "search empty string should rais exception" do
-      ProductElastic.index_all 
+      ProductElastic.index_all
       expect { ProductElastic.search("") }.to raise_error(ArgumentError)
     end
 
     # TODO: c gaves every c language, but c++ and c# dont work at all
     # Right now this case is not important because we are not tracking c++ or c#
-    it "test language filtering"  do 
+    it "test language filtering"  do
       sleep 4
       ProductElastic.search("club-mate", nil, ["Java"]).count.should eql(3)
     end
@@ -144,7 +147,7 @@ describe ProductElastic do
     #   results1.count.should eql(results2.count)
     # end
 
-    it " - should return 1 result with the right group_id." do 
+    it " - should return 1 result with the right group_id." do
       sleep 5
       results = ProductElastic.search nil, "com."
       results.count.should eql(1)
@@ -161,12 +164,12 @@ describe ProductElastic do
     it "test finding names, which includes points" do
       sleep 4
       results = ProductElastic.search "bad.mate.jar"
-      results.count.should eql(1)     
-    end 
+      results.count.should eql(1)
+    end
 
-    it "test finding names, which includes underscores" do 
+    it "test finding names, which includes underscores" do
       sleep 5
-      results = ProductElastic.search "superb_mate.jar"      
+      results = ProductElastic.search "superb_mate.jar"
       results.count.should eql(1)
     end
 
@@ -175,7 +178,7 @@ describe ProductElastic do
       ProductElastic.search_exact("club-mate").count.should eql(1)
       ProductElastic.search_exact("club-mate-c").count.should eql(1)
       ProductElastic.search_exact("club-mate-child").count.should eql(1)
-    end 
+    end
   end
-  
+
 end
