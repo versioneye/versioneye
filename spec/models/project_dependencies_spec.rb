b6434354557ada0660119bd7fbeeacf6c6859d8e
reiz <robert.reiz.81@gmail.com>
2013-06-09 18:02:33 +0200

    BugFix for project.create_key

--- spec/models/project_dependencies_spec.rb
+++ spec/models/project_dependencies_spec.rb
@@ -4,7 +4,7 @@ describe Project do
 
   before(:each) do
     @user       = UserFactory.create_new
-    @product    = ProductFactory.create_new
+    @product    = ProductFactory.create_new(1, :maven, true, "2.0.0")
     @project    = ProjectFactory.create_new( @user )
     @properties = Hash.new
   end
