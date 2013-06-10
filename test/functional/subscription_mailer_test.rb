5ec4c2c8698d00cf8c3f144a7dde94d491e7622a
reiz <robert.reiz.81@gmail.com>
2013-06-10 10:48:23 +0200

    Add database_cleaner. Tests refactored.

--- test/functional/subscription_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SubscriptionMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/api_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ApiTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
