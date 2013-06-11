d805055c14152b806b12d78e0b2eda9ea57cbb3d
reiz <robert.reiz.81@gmail.com>
2013-06-11 11:19:08 +0200

    Customize Landing page with clear focus and call to action.

--- app/assets/javascripts/custom_lp.js
+++ app/assets/javascripts/custom_lp.js
@@ -1,12 +1,24 @@
+
 jQuery(document).ready(function() {
-	
+
 	jQuery('#q').tbHinter({
 		text: "json",
 		styleclass: "lp_searchfield_hint"
 	});
-	
+
 });
 
 function load_video(){
   jQuery('#dialog_video').modal({keyboard : true});
+}
+
+function regulate_height(){
+  myHeight = window.innerHeight;
+  if (myHeight > 410) {
+    padding_height = myHeight - 419;
+    document.getElementById("search_section").style.cssText = "padding-bottom: " + padding_height + "px;";
+  } else {
+    document.getElementById("search_section").style.cssText = "padding-bottom: 65px";
+  }
+  setTimeout( function(){ regulate_height() }  , 1000 );
 }
