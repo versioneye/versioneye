881a7e7b8952cf3a119585be49859028a7aca3d9
reiz <robert.reiz.81@gmail.com>
2013-06-11 11:49:01 +0200

    Customize the second section of the landing page.

--- app/assets/javascripts/custom_lp.js
+++ app/assets/javascripts/custom_lp.js
@@ -16,7 +16,9 @@ function regulate_height(){
   myHeight = window.innerHeight;
   if (myHeight > 410) {
     padding_height = myHeight - 419;
+    reasons_height = (myHeight - 482) / 2;
     document.getElementById("search_section").style.cssText = "padding-bottom: " + padding_height + "px;";
+    document.getElementById("reasons").style.cssText = "padding-top: " + reasons_height + "px; padding-bottom: " + reasons_height +"px;";
   } else {
     document.getElementById("search_section").style.cssText = "padding-bottom: 65px";
   }
