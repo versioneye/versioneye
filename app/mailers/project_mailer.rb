839b338f49746b920aa96eb7e61fd82d295f5774
reiz <robert.reiz.81@gmail.com>
2013-06-18 15:17:53 +0200

    put the dependencies into a hash before sending out the project notification email.

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,19 +1,23 @@
 class ProjectMailer < ActionMailer::Base
-  
+
   default from: "\"VersionEye\" <notify@versioneye.com>"
-  
+
   def projectnotification_email( project )
-    @user = project.user
-    @dependencies = project.outdated_dependencies
+    @user         = project.user
+    deps          = project.outdated_dependencies
+    @dependencies = Hash.new
+    deps.each do |dep|
+      @dependencies[dep.name] = dep
+    end
     @project_name = project.name
-    @link = "#{Settings.server_url}/package/"
-    @projectlink = "#{Settings.server_url}/user/projects/#{project.id}"
+    @link         = "#{Settings.server_url}/package/"
+    @projectlink  = "#{Settings.server_url}/user/projects/#{project.id}"
     email = Project.email_for(project, @user)
     mail(
-      :to => email, 
+      :to => email,
       :subject => "Project Notification for #{project.name}",
       :tag => "project_notification"
       )
   end
-  
+
 end
