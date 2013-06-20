7d9a567516da289c3db2db2f9c2118a6b426e1c7
reiz <robert.reiz.81@gmail.com>
2013-06-20 16:30:53 +0200

    Seperate the config for factory girl and enforce ruby 1.9.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -31,5 +30,5 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  config.include FactoryGirl::Syntax::Methods
+
 end
