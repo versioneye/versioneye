906a4f5766cfc4af1d26cbcd7f0b208401c601ce
Timo Sulg <timgluz@gmail.com>
2013-06-25 19:52:27 +0200

    issue #74 - first version with latest, top libraris, userlist and vulnerability.

--- app/models/security_notification.rb
+++ app/models/security_notification.rb
@@ -1,30 +1,10 @@
 class SecurityNotification
   include Mongoid::Document
-  include Mongoid::Timestamps
 
-  field :cve_id, type: String
   field :summary, type: String
-  field :published, type: DateTime
-  field :last_modified, type: DateTime
 
-  field :score
+  field :metrics
+  belongs_to :product_security_notification
 
   #embeds_many :softwares
-
-  def self.parse_doc(doc)
-    entry = doc.read
-    if entry.root.name == "entry" and not entry.empty_element?
-      entry_doc = Nokogiri::XML(entry.inner_xml)
-      p entry_doc.xpath("//vulnerable-software-list")
-    end
-  end
-
-  def self.make_doc_reader(filepath)
-    # Open the file on disk and pass it to Nokogiri so that it can stream read;
-    f = File.open('./public/cves/nvdcve-2.0-2013.xml','r')
-    doc = Nokogiri::XML::Reader(f)
-    return doc
-  end
-
-
 end
