6f479612de1978ea8c1bec08dacbb5beaddb0de0
Timo Sulg <timgluz@gmail.com>
2013-07-04 16:53:40 +0200

    issue #74
    rebased to latest master
    supports now multiple languages
    removed redudant controllers
    updated routes

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,15 +1,18 @@
-class Languages::PythonController < ApplicationController
-  
-  def index
-    @lang = Product::A_LANGUAGE_PYTHON
+class LanguageController < ApplicationController
+
+  def show
+    sample_size = 24
+    max_population_size = 10 * sample_size
+    start_collecting_from = Random.rand(User.all.count - max_population_size)
+    @lang = Product.decode_language(params[:lang]) 
     @top_products = Product.by_language(@lang).desc(:followers).limit(10)
     @latest_products = Product.by_language(@lang).desc(:updated_at).limit(10)
     @followers = []
 
     #build sample population of followers 
-    population = []
-    Product.by_language(@lang).limit(1024).each do |product|
-      population.concat(product.users)
+    population = [] 
+    User.skip(start_collecting_from).each do |user|
+      population << user if user.products.where(language: @lang).exists?
       break if population.count >= 256
     end
 
@@ -21,7 +24,7 @@ class Languages::PythonController < ApplicationController
     end
 
     @feeds = LanguageFeed.by_language(@lang).map(&:url)
-    render template: "languages/index"
+    render template: "language/show"
+
   end
-  
 end
