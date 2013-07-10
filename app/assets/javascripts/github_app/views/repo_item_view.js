5a10e6ace9d7510bea083b2fdb63c19e552b443b
Timo Sulg <timgluz@gmail.com>
2013-07-10 16:11:56 +0200

    issue #102, issue #118, issue #119, issue #120
    Applied user's feedback to our GithubApp;
     * default ordering by pushed_at, by last commit
     * added option to filter by project accessability
     * shows branches by toggling
     * made height of branch-list higher

--- app/assets/javascripts/github_app/views/repo_item_view.js
+++ app/assets/javascripts/github_app/views/repo_item_view.js
@@ -10,18 +10,41 @@ define(['underscore', 'backbone',
 
     var GithubRepoItemView = Backbone.View.extend({
       template: _.template($("#github-repo-info-template").html()),
+      info_template: _.template($('#github-repo-control-info-template').html()),
+      events: {
+        "click .control-info-toggle": "toggleBranchView"
+      },
       render: function(){
         var control_view = new GithubRepoControlView({model: this.model});
-        var label_view = new GithubRepoLabelView({model: this.model});
-        
+        var label_view = new GithubRepoLabelView({model: this.model});   
         var repo_container = this.template({repo: this.model.toJSON()});
 
         this.$el.html(repo_container);
+        
+        var branch_info = this.info_template({
+          branches: this.model.get('branches'),
+          imported_branches: _.keys(this.model.get('imported_branches'))
+        });
+
+        this.$el.find(".repo-controls-info").html(branch_info);
+
         this.$el.find(".repo-controls").append(control_view.render().el);
         this.$el.find(".repo-labels").append(label_view.render().el);
 
         this.$el.find(".github-switch").bootstrapSwitch();
         return this;
+      },
+      toggleBranchView: function(ev){
+        var repo_controls = this.$el.find(".repo-controls");
+        
+        if(!repo_controls.hasClass("hide")){
+          $(ev.target).text("Show branches");
+        } else {
+          $(ev.target).text("Hide branches");
+        }
+
+        repo_controls.toggleClass("hide");
+        return false;
       }
     });
 
