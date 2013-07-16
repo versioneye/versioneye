1787c660b5abb401173ac4725cc5ef83dc0e8e5d
reiz <robert.reiz.81@gmail.com>
2013-07-16 18:14:58 +0200

    Fix for production assets precompile problem

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,21 @@ if defined?(Bundler)
 end
 
 
+class Settings
+    # raw_config = File.read("#{::Rails.root.to_s}/config/settings.yml")
+    raw_config = File.read("config/settings.yml")
+    erb_config = ERB.new(raw_config).result
+    settings = YAML.load(erb_config)[::Rails.env]
+
+    if settings
+        settings.each { |name, value|
+            instance_variable_set("@#{name}", value)
+            self.class.class_eval { attr_reader name.intern }
+        }
+    end
+end
+
+
 module Versioneye
   class Application < Rails::Application
 
--- config/environment.rb
+++ config/environment.rb
@@ -2,20 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 
-class Settings
-	raw_config = File.read("#{::Rails.root.to_s}/config/settings.yml")
-	erb_config = ERB.new(raw_config).result
-	settings = YAML.load(erb_config)[::Rails.env]
-
-	if settings
-		settings.each { |name, value|
-			instance_variable_set("@#{name}", value)
-			self.class.class_eval { attr_reader name.intern }
-		}
-	end
-end
-
-
 Versioneye::Application.initialize!
 
 
