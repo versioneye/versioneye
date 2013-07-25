507e8b5565cb656637400352bebe5b944c94c7fc
reiz <robert.reiz.81@gmail.com>
2013-07-25 16:28:11 +0200

    Optimization for language pages

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -7,7 +7,7 @@ class LanguageController < ApplicationController
   def show
     sample_size           = 24
     max_population_size   = 10 * sample_size
-    start_collecting_from = Random.rand(User.all.count - max_population_size)
+    start_collecting_from = 0 # Random.rand(User.all.count - max_population_size)
     @lang            = Product.decode_language(params[:lang])
     @top_products    = Product.by_language(@lang).desc(:followers).limit(10)
     @latest_products = Newest.by_language(@lang).desc(:created_at).limit(10)
@@ -16,13 +16,13 @@ class LanguageController < ApplicationController
 
     #build sample population of followers
     population = []
-    User.skip(start_collecting_from).each do |user|
+    User.all().each do |user|
       population << user if user.products.where(language: @lang).exists?
-      break if population.count >= 100
+      break if population.count >= 50
     end
 
     #pick random followers from population
-    @followers = population.sample(12)
+    @followers = population.sample(24)
 
     # lang_pattern = Regexp.new(@lang.downcase, true)
     # @vulnerabilities = SecurityNotification.all.in(languages: [lang_pattern]).desc(:modified).limit(30)
