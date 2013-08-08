de650b92687d02e7a407af6a037085e975eac46f
reiz <robert.reiz.81@gmail.com>
2013-08-08 17:05:32 +0200

    Testfix for lottery page

--- spec/features/lottery_authorization_spec.rb
+++ spec/features/lottery_authorization_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe 'lottery authorization' do
   let(:existing_user){FactoryGirl.create(:default_user)}
-  
-  describe "login with existing user credentials", :js => true do 
+
+  describe "login with existing user credentials", :js => true do
     it "should login with correct credentials and redirect to lottery page" do
       visit '/lottery'
       current_path.should == lottery_path
-    
+
       find('#collapse2_link').click #opens accordion
       fill_in 'session[email]',    with: existing_user[:email]
       fill_in 'session[password]', with: 'password'
       click_button 'Sign in'
 
       should_not have_css(".flash-container")
-      current_path.should == lucky_lottery_path
+      current_path.should == libraries_lottery_path
 
     end
   end
 
-  describe "signup as new user", :js => true do 
+  describe "signup as new user", :js => true do
     it "should signup with user data and redirect to lottery page" do
       User.all.delete_all
       visit '/lottery'
@@ -32,7 +32,7 @@ describe 'lottery authorization' do
 
       find('#btn-signup').click
 
-      current_path.should == lucky_lottery_path
+      current_path.should == libraries_lottery_path
       should_not have_css(".flash_container")
     end
   end
