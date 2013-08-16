c6d944800754349c3da01f45103b78bb66fcfad8
reiz <robert.reiz.81@gmail.com>
2013-08-16 20:59:04 +0200

    SSL for lottery page

--- app/controllers/lotteries_controller.rb
+++ app/controllers/lotteries_controller.rb
@@ -1,5 +1,7 @@
 class LotteriesController < ApplicationController
 
+  force_ssl if Rails.env.production?
+
   layout "lottery"
 
   before_filter :authenticate, :only => [:libraries, :follow, :thankyou]
