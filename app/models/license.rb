abfd42c9eacf5a409d1a422cd0041eefb50897bf
reiz <robert.reiz.81@gmail.com>
2013-08-20 00:36:55 +0200

    License Migration

--- app/models/license.rb
+++ app/models/license.rb
@@ -4,10 +4,17 @@ class License
   include Mongoid::Timestamps
 
   field :name         , type: String
-  field :identiy      , type: String
   field :url          , type: String
   field :comments     , type: String
   field :distributions, type: String
-  field :osi_approved , type: String
+
+  # This license belongs to the product with this attributes
+  field :language     , type: String
+  field :prod_key     , type: String
+  field :version      , type: String
+
+  def product
+    Product.fetch_product(self.language, self.prod_key)
+  end
 
 end
