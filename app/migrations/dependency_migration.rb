54ae291b0b6a2ab7d967d90550db7be6bc71beff
reiz <robert.reiz.81@gmail.com>
2013-08-26 15:22:11 +0200

    Remove old migrations

--- app/migrations/dependency_migration.rb
+++ app/migrations/dependency_migration.rb
@@ -7,43 +7,4 @@ class DependencyMigration
     end
   end
 
-  def self.update_php_prod_keys
-    elements = Dependency.where(:prod_key => /^php\//i)
-    elements.each do |element|
-      element.prod_key = element.prod_key.gsub("php\/", "")
-      element.save
-    end
-    elements = Dependency.where(:dep_prod_key => /^php\//i)
-    elements.each do |element|
-      element.dep_prod_key = element.dep_prod_key.gsub("php\/", "")
-      element.save
-    end
-  end
-
-  def self.update_pip_prod_keys
-    elements = Dependency.where(:prod_key => /^pip\//i)
-    elements.each do |element|
-      element.prod_key = element.prod_key.gsub("pip\/", "")
-      element.save
-    end
-    elements = Dependency.where(:dep_prod_key => /^pip\//i)
-    elements.each do |element|
-      element.dep_prod_key = element.dep_prod_key.gsub("pip\/", "")
-      element.save
-    end
-  end
-
-  def self.update_npm_prod_keys
-    elements = Dependency.where(:prod_key => /^npm\//i)
-    elements.each do |element|
-      element.prod_key = element.prod_key.gsub("npm\/", "")
-      element.save
-    end
-    elements = Dependency.where(:dep_prod_key => /^npm\//i)
-    elements.each do |element|
-      element.dep_prod_key = element.dep_prod_key.gsub("npm\/", "")
-      element.save
-    end
-  end
-
 end
