8bfcf5b7f966409c54a487922a8135f1188a7c21
Timo Sulg <timgluz@gmail.com>
2013-09-05 12:17:38 +0200

    issue #234 - updated latest_stats to use lang;

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -14,7 +14,7 @@ class LanguageController < ApplicationController
     @lang            = Product.decode_language(params[:lang])
     @top_products    = Product.by_language(@lang).desc(:followers).limit(10)
     @latest_products = Newest.by_language(@lang).desc(:created_at).limit(10)
-    @latest_stats     = LanguageDailyStats.latest_stats
+    @latest_stats     = LanguageDailyStats.latest_stats(@lang)
 
     @followers = []
     @languages = @@languages
