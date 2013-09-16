d58ef990e8f387c6c483f4583aa642a706fac2e7
reiz <robert.reiz.81@gmail.com>
2013-09-16 15:36:00 +0200

    clean up google code

--- app/assets/javascripts/google.js
+++ app/assets/javascripts/google.js
@@ -9,15 +9,6 @@ _gaq.push(['_trackPageview']);
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-32702672-1');  // Creates a tracker.
-// ga('set', 'anonymizeIp', true);
-// ga('send', 'pageview');
-
 function page_view(path){
   if (_gaq){
     _gaq.push(['_trackPageview', path]);
