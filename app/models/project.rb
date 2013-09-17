6930b275a12097841633bdb2bb5d7cbad6a4f74e
reiz <robert.reiz.81@gmail.com>
2013-09-17 12:12:27 +0200

    #245 small bugfixes.

--- app/models/project.rb
+++ app/models/project.rb
@@ -90,8 +90,10 @@ class Project
     return false
   end
 
+  # TODO write test for that.
   def collaborator( user )
     return nil if user.nil?
+    return user if self.user.username.eql?( user.username )
     return nil if collaborators.nil? || collaborators.empty?
     collaborators.each do |collaborator|
       return collaborator if user._id.to_s.eql?( collaborator.user_id.to_s )
