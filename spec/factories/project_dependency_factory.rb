1c88e1d3a6a4f7b2c4786f38731d82d9426094ec
Timo Sulg <timgluz@gmail.com>
2013-09-18 22:34:24 +0200

    issue #267 - tests for muting feature

--- spec/factories/project_dependency_factory.rb
+++ spec/factories/project_dependency_factory.rb
@@ -1,7 +1,8 @@
 
 FactoryGirl.define do
   factory :projectdependency do
-    prod_key ""
+    prod_key "spec_product1"
+    language "Ruby"
     name ""
     version_current "0.1"
     version_requested "0.1"
