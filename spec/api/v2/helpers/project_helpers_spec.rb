a50473c3c458159820a8b3abfef82acf717bfabb
reiz <robert.reiz.81@gmail.com>
2013-10-06 12:07:55 -0400

    Testfix

--- spec/api/v2/helpers/project_helpers_spec.rb
+++ spec/api/v2/helpers/project_helpers_spec.rb
@@ -13,6 +13,7 @@ describe ProjectHelpers do
       helper.extend(ProjectHelpers)
       product1.save
       product2.save
+      licence2.version = product2.version
       licence2.save
     end
 
@@ -22,9 +23,11 @@ describe ProjectHelpers do
 
     it "returns correct licence, when product has one" do
       project.dependencies.first.save
+      project.dependencies.last.save
       project_after = helper.add_dependency_licences(project)
 
       project_after.should_not be_nil
+      project_after.dependencies.size.should eq(2)
 
       dep1 = project_after.dependencies[0]
       dep1.product.should_not be_nil
@@ -34,7 +37,7 @@ describe ProjectHelpers do
       dep2 = project_after.dependencies[1]
       dep2.product.should_not be_nil
       dep2.product.license_info.should_not be_nil
-      dep2.product.license_info.should eql("MIT, unknown")
+      dep2.product.license_info.should eql("MIT")
     end
   end
 end
