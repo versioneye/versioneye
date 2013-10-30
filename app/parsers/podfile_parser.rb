6d875ce69c18c2b4495e18acf27b26874874721b
Richard Metzler <richard@paadee.com>
2013-10-30 10:01:42 +0100

    basic project

--- app/parsers/podfile_parser.rb
+++ app/parsers/podfile_parser.rb
@@ -1,8 +1,38 @@
 require 'cocoapods-core'
 
+# This parser parses the podfile for any CocoaPods project
+# and creates and stores a Project model in VersionEye
+#
+# TODO doc url
+
 class PodFileParser < CommonParser
 
   #Pod::Podfile
   #Pod::Lockfile
-  
+
+  @@project_type = Project::A_TYPE_COCOAPODS
+  @@language     = Product::A_LANGUAGE_OBJECTIVEC
+
+  def initialize
+    # TODO not sure what to put here
+  end
+
+  def parse_file file
+    @podfile = load_podfile file
+    @project = get_project
+    @project
+  end
+
+  def load_podfile file
+    Pod::Podfile.new file
+  end
+
+  def get_project
+    project = Project.new project_type: @@project_type, language:@@language
+
+    project.save
+    project
+  end
+
+
 end
--- spec/parsers/podfile_parser_spec.rb
+++ spec/parsers/podfile_parser_spec.rb
@@ -4,8 +4,13 @@ describe PodFileParser do
 
   describe '#parse' do
 
-    it "should read a podfile and return project, dependencies" do
+    it "should read a simple podfile and return project, dependencies" do
+      parser = PodFileParser.new
+      project = parser.parse_file './spec/fixture/files/podfile/very-simple.podfile'
 
+      project.should be_true
+      project.language.should eq Product::A_LANGUAGE_OBJECTIVEC
+      project.project_type.should eq Project::A_TYPE_COCOAPODS
     end
 
   end
