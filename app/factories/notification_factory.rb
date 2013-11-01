f90977b19549ec8f31ab7fb08cb73087ae3a5c34
Timo Sulg <timgluz@gmail.com>
2013-11-01 11:29:18 +0100

    issue #286 - added collaborated projects; changed product_project_index;

--- app/factories/notification_factory.rb
+++ app/factories/notification_factory.rb
@@ -2,7 +2,7 @@ class NotificationFactory
 
   def self.create_new(user = nil, save = true)
     user = UserFactory.create_new if user.nil?
-    random_product = ProductFactory.create_new(1, :gemfile)
+    random_product = ProductFactory.create_new(Random.rand(100000), :gemfile)
     new_message = Notification.new  user_id: user.id,
                                     product_id: random_product.id,
                                     version_id: random_product.versions.last.id,
@@ -15,10 +15,13 @@ class NotificationFactory
 
     new_message
   end
-  
+
   def self.create_new_for_user_project(user = nil, save = true)
     user = UserFactory.create_new if user.nil?
-    prod1 = Project.by_user(user).first.dependencies.to_a.sample
+    project = Project.by_user(user).to_a.sample
+    #project = Project.by_collaborator(user).to_a.sample
+
+    prod1 = project.dependencies.to_a.sample
     random_product = Product.fetch_product(prod1[:language], prod1[:prod_key])
     new_message = Notification.new  user_id: user.id,
                                     product_id: random_product.id,
