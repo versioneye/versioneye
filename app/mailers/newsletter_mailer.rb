2e5f0f036620391b995bb73f5d72bce991d80e49
reiz <robert.reiz.81@gmail.com>
2013-11-06 10:17:28 +0100

    New Features email

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -6,7 +6,7 @@ class NewsletterMailer < ActionMailer::Base
     @user = user
     mail(
       :to => @user.email,
-      :subject => "New Features - New API Endpoints and more speed.",
+      :subject => "New Features - OpenSearch + More Speed.",
       :tag => "newsletter"
       )
   end
