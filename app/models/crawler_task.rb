dd3339e79557212e0c62e22aa13d9c9be910ee97
Timo Sulg <timgluz@gmail.com>
2013-11-19 20:58:12 +0200

    issue #276 - refactored BowerCrawler
    changed model persitance - prevent reduntant data and removing old data;
    added github.io as supported url;
    refactored crawler to 2separated workers;
    clean ups;

--- app/models/crawler_task.rb
+++ app/models/crawler_task.rb
@@ -7,18 +7,21 @@ class CrawlerTask
 
   field :runs,          type: Integer, default: 0 #how many times crawled
   field :fails,         type: Integer, default: 0 #how many times failed
-  field :task_name,     type: String #use format <crawlers_name>/<task_name>
-  field :task_done,     type: Boolean, default: false 
-  field :recrawl,       type: Boolean, default: false 
+  field :task_target,   type: String #use format <crawlers_name>/<task_name>
+  field :task_source,   type: String 
+  field :task_failed,   type: Boolean, default: false
+  field :re_crawl,      type: Boolean, default: false 
   field :source,        type: String #github | plain url | ... 
   field :url,           type: String
   field :url_params,    type: Hash
+  field :url_exists,    type: Boolean, default: false
   field :repo_owner,    type: String
   field :repo_name,     type: String
   field :repo_fullname, type: String
-  field :repo_weight,   type: String #weight = stars + watchers 
-  field :exists,        type: Boolean, default: false
+  field :repo_weight,   type: Integer, default: 0 #weight = stars + watchers 
   field :data,          type: Hash #subdocument to keep pre-cached data
 
-  scope :by_task, ->(name){where(task_name: name)}
+  scope :by_task, ->(name){where(task_target: name)}
+  scope :re_crawlable, where(re_crawl: true, task_failed: false, url_exists: true)
+
 end
