43fcc7d329844476fafb9f8b7113c2aea7fb5692
Richard Metzler <richard.metzler@me.com>
2013-11-19 10:43:08 +0100

    start the parser

---
+++ app/parsers/podlockfile_parser.rb
@@ -0,0 +1,51 @@
+require 'cocoapods-core'
+
+class PodfilelockParser < CommonParser
+
+  #Pod::Lockfile
+
+  def parse_file filename
+    @lock_filename = filename
+    file = File.new @lock_filename
+    @lock_file = Pod::Lockfile.from_file file
+    create_project
+  end
+
+  def create_project
+    @project = init_project
+    create_dependencies
+    @project
+  end
+
+  def init_project
+    Project.new \
+      project_type: Project::A_TYPE_COCOAPODS,
+      language: Product::A_LANGUAGE_OBJECTIVEC,
+      url: @url
+  end
+
+  def create_project
+    @pod_file.dependencies.each do |d|
+      create_dependency d.name => d.requirement.as_list
+    end
+
+    @project.dep_number = @project.projectdependencies.count
+    Rails.logger.info "Project has #{@project.projectdependencies.count} dependencies"
+  end
+
+  def create_dependency dep
+    if dep.nil?
+      Rails.logger.debug "Problem: try to create_dependency(nil)"
+      return nil
+    end
+
+    dependency = create_dependency_from_hash( dep )
+    @project.out_number     += 1 if dependency.outdated?
+    @project.unknown_number += 1 if dependency.prod_key.nil?
+    @project.projectdependencies.push dependency
+    dependency.save
+    dependency
+  end
+
+
+end
--- spec/parsers/podfilelock_parser_spec.rb
+++ spec/parsers/podfilelock_parser_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
-describe PodLockFileParser do
+describe PodfilelockParser do
 
   describe '#parse' do
 
-    it "should read a lockfile and return project, dependencies" do
-      
+    it "should HTTP-get a lockfile and return a project with dependencies" do
+    	project = 
+    end
+
+  end
+
+  describe '#parse_file' do
+
+    it "should read a lockfile from disk and return a project with dependencies" do
 
     end
 
