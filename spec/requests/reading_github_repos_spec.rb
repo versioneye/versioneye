b5aff82aacf4a913680820ad47a4aa8e5ee04d1d
Timo Sulg <timgluz@gmail.com>
2013-11-20 21:04:44 +0200

    issue #319 - debugging reading_github_repos_spec;

--- spec/requests/reading_github_repos_spec.rb
+++ spec/requests/reading_github_repos_spec.rb
@@ -3,21 +3,36 @@ require 'spec_helper'
 describe "Getting data from github_repos_controller" do
   let(:user) {create(:user, username: "pupujuku", fullname: "Pupu Juku", email: "juku@pupu.com")}
 
-  let(:repo1) {create(:github_repo, user_id: user.id.to_s, github_id: 1, fullname: "spec/repo1",
-                      user_login: "a", owner_login: "versioneye", owner_type: "user")}
-  let(:repo2) {create(:github_repo, user_id: user.id.to_s, github_id: 2, fullname: "spec/repo2",
-                      user_login: "a", owner_login: "versioneye", owner_type: "user")}
+  let(:repo1) {create(:github_repo, 
+                      user_id: user.id.to_s, 
+                      github_id: 1, 
+                      fullname: "spec/repo1",
+                      user_login: "a", 
+                      owner_login: "versioneye", 
+                      owner_type: "user",
+                     )
+                }
+  let(:repo2) {create(:github_repo, 
+                      user_id: user.id.to_s, 
+                      github_id: 2, 
+                      fullname: "spec/repo2",
+                      user_login: "a", 
+                      owner_login: "versioneye", 
+                      owner_type: "user",
+                     )
+              }
 
   before :each do
-    GithubRepo.delete_all
+    #GithubRepo.delete_all
 
     get signin_path, nil, "HTTPS" => "on"
     post sessions_path, {session: {email: user.email, password: "12345"}}, "HTTPS" => "on"
     assert_response 302
     response.should redirect_to( user_packages_i_follow_path )
-
+    user.save
     repo1.save
     repo2.save
+    user.github_repos << repo1
   end
 
   after :each do
@@ -28,10 +43,12 @@ describe "Getting data from github_repos_controller" do
   describe "getting list of repos" do
     it "should return list of repos when repos are already cached" do
       user.github_repos.all.count.should > 0 #factory should fill GithubRepo
+      p "Repo from spec: ", user.github_repos.first
 
       get user_github_repos_path
       response.status.should eql(200)
-
+      
+      p response.body
       response_data = JSON.parse response.body
       response_data.has_key?('repos').should be_true
       response_data['repos'].count.should eq(user.github_repos.count)
