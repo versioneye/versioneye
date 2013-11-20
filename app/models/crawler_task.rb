e7e5e1fecfe3ea880434868f63b5d12d87c3a027
Timo Sulg <timgluz@gmail.com>
2013-11-20 17:52:09 +0200

    #276 - fixes for crawler;

--- app/models/crawler_task.rb
+++ app/models/crawler_task.rb
@@ -2,16 +2,11 @@ class CrawlerTask
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  A_SOURCE_GITHUB   = "github"
-  A_SOURCE_URL      = "url"
-
   field :runs,          type: Integer, default: 0 #how many times crawled
   field :fails,         type: Integer, default: 0 #how many times failed
-  field :task_target,   type: String #use format <crawlers_name>/<task_name>
-  field :task_source,   type: String 
+  field :task,          type: String #use format <crawlers_name>/<task_name>
   field :task_failed,   type: Boolean, default: false
   field :re_crawl,      type: Boolean, default: false 
-  field :source,        type: String #github | plain url | ... 
   field :url,           type: String
   field :url_params,    type: Hash
   field :url_exists,    type: Boolean, default: false
@@ -21,7 +16,7 @@ class CrawlerTask
   field :repo_weight,   type: Integer, default: 0 #weight = stars + watchers 
   field :data,          type: Hash #subdocument to keep pre-cached data
 
-  scope :by_task, ->(name){where(task_target: name)}
+  scope :by_task, ->(name){where(task: name)}
   scope :re_crawlable, where(re_crawl: true, task_failed: false, url_exists: true)
 
 end
