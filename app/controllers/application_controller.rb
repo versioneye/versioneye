f8831692370fff61438368a11770b9bbda239931
reiz <robert.reiz.81@gmail.com>
2013-11-28 10:24:56 +0100

    Refactor the refer logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  
+
   protect_from_forgery
-  
+
   include SessionsHelper
   include ProductsHelper
   include UsersHelper
   include ProjectsHelper
-  
+  include RefersHelper
+
 end
