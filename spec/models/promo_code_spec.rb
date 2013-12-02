16b7fc5f226811fe040e25c565eb8435fcfb1d35
reiz <robert.reiz.81@gmail.com>
2013-12-02 11:33:28 +0100

    Adding behavior to promocode model. #362.

--- spec/models/promo_code_spec.rb
+++ spec/models/promo_code_spec.rb
@@ -9,12 +9,47 @@ describe PromoCode do
     end
 
     it "returns promo tada for nil" do
-      PromoCode.new({:name => "tada", :free_private_projects => 3}).save
-      tada = PromoCode.by_name("tada")
+      now = DateTime.now
+      future = now + 7.days
+      PromoCode.new({:name => "changelog_weekly_1", :free_private_projects => 3, :end_date => future}).save
+      tada = PromoCode.by_name("changelog_weekly_1")
       tada.should_not be_nil
-      tada.name.should eq("tada")
+      tada.name.should eq("changelog_weekly_1")
       tada.free_private_projects.should eq(3)
       tada.redeemed.should eq(0)
+      tada.end_date.should_not be_nil
+      tada.end_date.should eq(future)
+    end
+
+  end
+
+  describe "valid?" do
+
+    it "is valid" do
+      promo = PromoCode.new({:name => "changelog_weekly_1", :free_private_projects => 3})
+      promo.save
+      promo.is_valid?().should be_true
+    end
+
+    it "is not valid" do
+      past = DateTime.now - 7.days
+      promo = PromoCode.new({:name => "changelog_weekly_1", :free_private_projects => 3, :end_date => past})
+      promo.save
+      promo.is_valid?().should be_false
+    end
+
+  end
+
+  describe "redeem!" do
+
+    it "remeems" do
+      promo = PromoCode.new({:name => "changelog_weekly_2", :free_private_projects => 3})
+      promo.save
+      promo.redeemed.should eq(0)
+      promo.redeem!
+      promo.redeemed.should eq(1)
+      promo.redeem!
+      promo.redeemed.should eq(2)
     end
 
   end
