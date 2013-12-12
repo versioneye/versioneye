d5d65545b9e3245cdd48cc9fba818304f068195f
reiz <robert.reiz.81@gmail.com>
2013-12-12 13:02:15 +0100

    Add sorting and grouping to the license table view

--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -36,5 +36,36 @@ describe License do
 
   end
 
+  describe "name_substitute" do
+
+    it "should return MIT name" do
+      license = License.new({:name => "MIT"})
+      license.name_substitute.should eq("MIT")
+    end
+    it "should return MIT name" do
+      license = License.new({:name => "MIT License"})
+      license.name_substitute.should eq("MIT")
+    end
+    it "should return MIT name" do
+      license = License.new({:name => "The MIT License"})
+      license.name_substitute.should eq("MIT")
+    end
+
+    it "should return Apache License version 2 name" do
+      license = License.new({:name => "The Apache Software License\, Version 2\.0"})
+      license.name_substitute.should eq("Apache License, Version 2.0")
+    end
+    it "should return Apache License version 2 name" do
+      license = License.new({:name => "Apache License, Version 2.0"})
+      license.name_substitute.should eq("Apache License, Version 2.0")
+    end
+    it "should return Apache License version 2 name" do
+      license = License.new({:name => "Apache License Version 2.0"})
+      license.name_substitute.should eq("Apache License, Version 2.0")
+    end
+
+
+  end
+
 end
 
