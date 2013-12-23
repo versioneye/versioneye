0d3dc0c3caf7e2a6673da546e2459e3230ffe525
Timo Sulg <timgluz@gmail.com>
2013-12-23 19:29:23 +0100

    #397 - test fixes;

--- spec/features/signup_bitbucket_spec.rb
+++ spec/features/signup_bitbucket_spec.rb
@@ -2,18 +2,30 @@ require 'spec_helper'
 describe "Signup with Bitbucket" do
   let(:user1){FactoryGirl.build(:user, email: "test@versioneye.com")}
 
+  before :each do
+    User.all.delete_all
+    visit 'https://bitbucket.org/account/signout/'
+    page.has_content? 'Unlimited private code repositories'
+  end
+
+  after :each do
+    visit 'https://bitbucket.org/account/signout/'
+    page.has_content? 'Unlimited private code repositories'
+  end
+
+
   it "signup a new user with Bitbucket", js: true do
     visit signup_path
     page.has_css? 'button.btn-bitbucket'
     click_button "Login with Bitbucket"
 
-    #log in with testuser's credentials
+    #log into Bitbucket when required
+    find("form.login-form").visible?
     within("form.login-form") do
       fill_in "Username", :with => Settings.bitbucket_username
       fill_in 'Password', :with => Settings.bitbucket_password
       click_button 'Log in'
     end
-    
     #grant access
     if page.has_css? 'button.aui-button'
       click_button "Grant access"
