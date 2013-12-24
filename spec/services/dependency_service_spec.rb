a17f88fe3e4f612a97725d7305869453c5f9eff8
reiz <robert.reiz.81@gmail.com>
2013-12-24 15:20:06 +0100

    Remove unused variables

--- spec/services/dependency_service_spec.rb
+++ spec/services/dependency_service_spec.rb
@@ -80,10 +80,10 @@ describe DependencyService do
       prod_3 = ProductFactory.create_new(3)
       prod_4 = ProductFactory.create_new(4)
 
-      dep_1 = DependencyFacotry.create_new(product, prod_1)
-      dep_2 = DependencyFacotry.create_new(product, prod_2)
-      dep_3 = DependencyFacotry.create_new(product, prod_3)
-      dep_4 = DependencyFacotry.create_new(product, prod_4)
+      DependencyFacotry.create_new(product, prod_1)
+      DependencyFacotry.create_new(product, prod_2)
+      DependencyFacotry.create_new(product, prod_3)
+      DependencyFacotry.create_new(product, prod_4)
 
       product.dependencies(nil).size.should eq(4)
       DependencyService.dependencies_outdated?( product.dependencies(nil) ).should be_false
@@ -102,9 +102,9 @@ describe DependencyService do
       prod_3 = ProductFactory.create_new(3)
       prod_4 = ProductFactory.create_new(4)
 
-      dep_1 = DependencyFacotry.create_new(product, prod_1)
-      dep_2 = DependencyFacotry.create_new(product, prod_2)
-      dep_3 = DependencyFacotry.create_new(product, prod_3)
+      DependencyFacotry.create_new(product, prod_1)
+      DependencyFacotry.create_new(product, prod_2)
+      DependencyFacotry.create_new(product, prod_3)
       dep_4 = DependencyFacotry.create_new(product, prod_4)
       dep_4.version = "0.0"
       dep_4.save
