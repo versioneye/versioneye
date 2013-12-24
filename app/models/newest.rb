aca263fa698f515a815adef44678aa58c4a3658d
reiz <robert.reiz.81@gmail.com>
2013-12-24 01:18:57 +0100

    #236 remove p logging

--- app/models/newest.rb
+++ app/models/newest.rb
@@ -24,8 +24,6 @@ class Newest
 
   def self.fetch_newest language, prod_key, version
     Newest.where(:language => language, :prod_key => prod_key, :version => version).shift
-  rescue => e
-    p e
   end
 
   def self.get_newest( count )
