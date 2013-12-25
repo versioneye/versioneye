6bf0c5100e6f1325913b99f98a6e1de7c32b5132
Timo Sulg <timgluz@gmail.com>
2013-12-25 16:39:34 +0100

    #397 - added specs for services/bitbucket;

--- spec/features/signin_bitbucket_spec.rb
+++ spec/features/signin_bitbucket_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
+def connect_user_account(user)
+  describe "connect" do
+    it 'connects user with bitbucket', js: true do
+      visit signin_path
+      page.has_css? 'button.btn-bitbucket'
+      click_button "Login with Bitbucket"
+
+      #when bitbucket asks testuser's credentials
+      within("form.login-form") do
+        fill_in "Username", :with => Settings.bitbucket_username
+        fill_in 'Password', :with => Settings.bitbucket_password
+        click_button 'Log in'
+      end
+      #grant access
+      if page.has_css? 'button.aui-button'
+        click_button "Grant access"
+      end
+    end
+  end
+
+  User.where(email: user[:email]).shift #returns updated user
+end
+
 describe "Signin with Bitbucket" do
   let(:user1){FactoryGirl.create(:bitbucket_user,
                                  bitbucket_id: "versioneye_test")}
