5715a0a50fdfa81c7d3c99618a176986e8a005c8
reiz <robert.reiz.81@gmail.com>
2014-01-04 16:08:50 +0100

    Remove unused code

--- spec/requests/signup_github_spec.rb
+++ spec/requests/signup_github_spec.rb
@@ -54,22 +54,6 @@ describe "SignUp with GitHub" do
     end
   end
 
-  it "signin an existing user with GitHub. The email address is already in the database." do
-    user = UserFactory.create_new
-
-    FakeWeb.register_uri(:get, "https://github.com/login/oauth/access_token?client_id=#{Settings.github_client_id}&client_secret=#{Settings.github_client_secret}&code=123", :body => "token=token_123")
-    FakeWeb.register_uri(:get, %r|https://api.github.com/user*|, 
-                         :body => "{\"id\": 1585858, \"email\": \"#{user.email}\"}")
-
-    get "/auth/github/callback?code=123"
-    assert_response 302
-    response.should redirect_to( user_packages_i_follow_path )
-
-    user_db = User.find_by_email( user.email )
-    user_db.github_token.should eql("token_123")
-    user_db.github_id.should eql("1585858")
-  end
-
   it "connect a signed in user to his GitHub Account." do
     user = UserFactory.create_new
     user.github_id = nil
