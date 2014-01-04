ce026094688d566886a5942aea865691da83b8ea
reiz <robert.reiz.81@gmail.com>
2014-01-04 16:32:00 +0100

    Remove public assets

--- public/assets/scm_app/collections/menu_collection.js
+++
@@ -1,1 +0,0 @@
-define(["underscore","backbone"],function(e,t){e.templateSettings={interpolate:/\{\{\=(.+?)\}\}/g,evaluate:/\{\{(.+?)\}\}/g};var i=t.Model.extend({}),n=t.Collection.extend({url:"/urls/menu",model:i,initialize:function(e,t){this.url=t.urls.menu}});return n});
