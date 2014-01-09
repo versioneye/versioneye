0ae2320a390f70c73bb62b9ea781573dce246a12
reiz <robert.reiz.81@gmail.com>
2014-01-09 18:30:17 +0100

    comment out custom precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,18 +55,18 @@ namespace :deploy do
     end
   end
 
-  desc 'assets:precompile'
-  namespace :assets do
-    task :precompile do
-      on roles :app, in: :sequence, wait: 5 do
-        within release_path do
-          with rails_env: fetch(:rails_env) do
-            execute :rake, "assets:precompile"
-          end
-        end
-      end
-    end
-  end
+  # desc 'assets:precompile'
+  # namespace :assets do
+  #   task :precompile do
+  #     on roles :app, in: :sequence, wait: 5 do
+  #       within release_path do
+  #         with rails_env: fetch(:rails_env) do
+  #           execute :rake, "assets:precompile"
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 
   after :finishing, 'deploy:cleanup'
 
