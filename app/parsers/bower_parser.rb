5b6fa0446cf15aa269a90ab11d31896c1c4821ac
Timo Sulg <timgluz@gmail.com>
2014-01-23 20:04:37 +0100

    remove reduntant prod_type  in init_dependency;

--- app/parsers/bower_parser.rb
+++ app/parsers/bower_parser.rb
@@ -277,7 +277,6 @@ class BowerParser < CommonParser
     dependency          = Projectdependency.new
     dependency.name     = name
     dependency.language = Product::A_LANGUAGE_JAVASCRIPT
-    dependency.prod_type = Project::A_TYPE_BOWER
 
     if product
       dependency.prod_key        = product.prod_key
