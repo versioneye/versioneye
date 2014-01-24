fd796d3b70bed3dc90bda874bd3d4d695e31e092
reiz <robert.reiz.81@gmail.com>
2014-01-24 21:14:14 +0100

    Add language constraint

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -6,7 +6,7 @@ class LanguageConstraint
     languages = Product::A_LANGS_ALL
     languages << "nodejs"
     Product::A_LANGS_ALL.each do |lang|
-      return true if path.match(/^#{lang}/i)
+      return true if path.match(/^#{lang}$/i) || path.match(/^#{lang}\/.*$/i)
     end
     false
   end
