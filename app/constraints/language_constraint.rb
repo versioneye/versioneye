b1375cf7fe5d9c71fc69bba7a5c63c5b63759d97
reiz <robert.reiz.81@gmail.com>
2014-01-28 23:20:45 +0100

    Allow all languages in the language constraint we have in our db

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -5,7 +5,7 @@ class LanguageConstraint
     path = path[1..path.length]
     languages = Product::A_LANGS_ALL
     languages << "nodejs"
-    Product::A_LANGS_ALL.each do |lang|
+    Product.all.distinct(:language).each do |lang|
       return true if path.match(/^#{lang}$/i) || path.match(/^#{lang}\/.*$/i)
     end
     false
