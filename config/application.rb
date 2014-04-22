69e82fbbb8dd1d5544d8a0e7f4b36cf4aecacc09
reiz <robert.reiz.81@gmail.com>
2014-04-22 17:44:08 +0200

    Add assets to enterprise env

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require 'tire'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require *Rails.groups(:assets => %w(development test enterprise))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
