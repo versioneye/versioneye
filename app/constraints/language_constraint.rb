4edac197a5a4ff12c9d4412063cbdb93af52f4a9
reiz <robert.reiz.81@gmail.com>
2014-05-19 21:50:53 +0200

    Add cache for distint languages

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -3,7 +3,7 @@ class LanguageConstraint
   def matches?(request)
     path = request.path
     path = path[1..path.length]
-    languages = Product.all.distinct(:language)
+    languages = distinct_languages
     languages << 'nodejs'
     languages.each do |lang|
       return true if /^#{lang}$/i =~ path || /^#{lang}\/.*$/i =~ path
@@ -11,5 +11,17 @@ class LanguageConstraint
     end
     false
   end
+
+  private
+
+    def distinct_languages
+      key = "distinct_languages"
+      languages = Rails.cache.read key
+      if languages.to_s.empty?
+        languages = Product.all.distinct(:language)
+        Rails.cache.write( key, languages, timeToLive: 1.hour )
+      end
+      languages
+    end
 
 end
