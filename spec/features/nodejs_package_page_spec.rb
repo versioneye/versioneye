42871d8de8062a123d59408bafea3c9af632fcb6
reiz <robert.reiz.81@gmail.com>
2014-05-22 22:24:03 +0200

    Little refactoring for tests

--- spec/features/nodejs_package_page_spec.rb
+++ spec/features/nodejs_package_page_spec.rb
@@ -4,20 +4,20 @@ describe "NodeJS path tester" do
 
   describe "testing nodejs paths on VersionEye", :js => true do
 
-    it "visit nodejs language page with dot in lang" do
-      prod1 = ProductFactory.create_for_npm('json', '1.0.0')
-      prod1.save
-
-      visit "/node.js"
-      page.should have_content("json")
-    end
-
     it "shows the nodejs page" do
       prod1 = ProductFactory.create_for_npm('json', '1.0.0')
       prod1.save
 
       visit "/nodejs/json/1.0.0"
       page.should have_content("json : 1.0.0")
+    end
+
+    it "visit nodejs language page with dot in lang" do
+      prod1 = ProductFactory.create_for_npm('json', '1.0.0')
+      prod1.save
+
+      visit "/node.js"
+      page.should have_content("json")
     end
 
     it "shows the nodejs page with a dot in the language name" do
@@ -31,4 +31,3 @@ describe "NodeJS path tester" do
   end
 
 end
-
