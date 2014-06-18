c57d2dae1a81ca26e2fec861996dc7b3cdefa712
reiz <robert.reiz.81@gmail.com>
2014-06-18 02:18:28 +0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,16 @@ require File.expand_path('../application', __FILE__)
 
 Versioneye::Application.initialize!
 
+es_url = Settings.instance.elasticsearch_url
+if !Settings.instance.elasticsearch_addr.to_s.empty? && !Settings.instance.elasticsearch_port.to_s.empty?
+  es_url = "#{Settings.instance.elasticsearch_addr}:#{Settings.instance.elasticsearch_port}"
+end
 
 begin
   Tire.configure do
     # logger STDOUT
     # logger STDERR
-    url Settings.instance.elasticsearch_url ||= "#{Settings.instance.elasticsearch_addr}:#{Settings.instance.elasticsearch_port}"
+    url es_url
   end
 rescue => e
   Rails.logger.error "Wrong configuration: #{e}"
