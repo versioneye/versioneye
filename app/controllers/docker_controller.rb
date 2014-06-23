8d31e813e92bb37dcbea9b6f0b04820c7128cd1b
reiz <robert.reiz.81@gmail.com>
2014-06-24 00:57:59 +0200

    Add new remote images

--- app/controllers/docker_controller.rb
+++ app/controllers/docker_controller.rb
@@ -17,20 +17,49 @@ class DockerController < ApplicationController
     def remote_images_hash
       images = {}
 
-      images['reiz/memcached:1.0.0']     = {
-        'container_start_opts' => {'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}},
-        'comments' => 'First version' }
-
-      images['reiz/mongodb:1.0.0']       = {
-        'container_start_opts' => {'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]},
-        'Binds' => ['/mnt/mongodb:/data']},
-        'comments' => 'First version' }
-
-      images['reiz/elasticsearch:1.0.1'] = {
-        'container_start_opts' => {'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9200'}]},
-        'Binds' => ['/mnt/elasticsearch:/data']},
+      images['veye/rails_app:1.0.0'] = {
+        'container_start_opts' => {
+          'PortBindings' => { '8080/tcp' => [{'HostPort' => '8080'}]},
+          'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc']},
         'comments' => 'First version'
       }
+
+      images['veye/rails_api:1.0.0'] = {
+        'container_start_opts' => {
+          'PortBindings' => { '9090/tcp' => [{'HostPort' => '9090'}]},
+          'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc']},
+        'comments' => 'First version'
+      }
+
+      images['veye/crawlr:1.0.0'] = {
+        'container_start_opts' => {
+          'Links' => ['mongodb:db']},
+          'Binds' => ['/mnt/cocoapods:/mnt/cocoapods']},
+        'comments' => 'First version'
+      }
+
+      images['reiz/mongodb:1.0.0'] = {
+        'container_start_opts' => {
+          'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]},
+          'Binds' => ['/mnt/mongodb:/data']},
+        'comments' => 'First version'
+      }
+
+      images['reiz/elasticsearch:1.0.1'] = {
+        'container_start_opts' => {
+          'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9200'}]},
+          'Binds' => ['/mnt/elasticsearch:/data']
+          },
+        'comments' => 'First version'
+      }
+
+      images['reiz/memcached:1.0.0'] = {
+        'container_start_opts' => {
+          'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}
+          },
+        'comments' => 'First version'
+      }
+
       images
     end
 
