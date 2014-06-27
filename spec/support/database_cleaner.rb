08302754a46414494b33b46b5bc76c39439342c3
reiz <robert.reiz.81@gmail.com>
2014-06-27 14:14:56 +0200

    Create indexes before test suite starts

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -5,6 +5,8 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.orm = "mongoid"
+    Indexer.drop_indexes
+    Indexer.create_indexes
   end
 
   config.before(:each) do
