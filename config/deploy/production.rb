8600f57248115c4d25854154175d7dfbecb62cad
reiz <robert.reiz.81@gmail.com>
2014-07-02 10:06:23 +0200

    Customize the deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :rails_env, :production
 
 set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
 
-role :app, %w{ubuntu@veye_www}
+role :app, %w{ubuntu@veye_app_1}
 
 set :ssh_options, {
    forward_agent: true # , auth_methods: %w(password)
