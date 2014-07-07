65a1556fae65bf6f4e5ca616e75db1816c2c9bfb
reiz <robert.reiz.81@gmail.com>
2014-07-07 16:39:38 +0200

    Ensure indexes in daily job

--- config/application.rb
+++ config/application.rb
@@ -70,7 +70,6 @@ module Versioneye
     begin
       p "application.rb ... instantiate VersioneyeCore ... "
       VersioneyeCore.new
-      # Indexer.create_indexes
     rescue => e
       p e.message
       p e.backtrace.join("\n")
