833da6d7baacee61f7c9d7379dae134ead15566e
reiz <robert.reiz.81@gmail.com>
2014-07-08 17:23:02 +0200

    Update gems

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :rails_env, :production
 
 set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
 
-role :app, %w{ubuntu@veye_app_1}
+role :app, %w{ubuntu@54.72.33.230}
 
 set :ssh_options, {
    forward_agent: true # , auth_methods: %w(password)
