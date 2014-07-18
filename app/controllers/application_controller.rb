123bfef82c9698a99a09590c37835dad4c41a4ec
reiz <robert.reiz.81@gmail.com>
2014-07-18 17:34:11 +0200

    Explicitly close connections to DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
   include ProjectsHelper
   include RefersHelper
 
+  after_filter :disconnect
+
+  # Explicitly close the connection to MongoDB.
+  # This is a bug in MongoID 3.X.
+  # http://grokbase.com/t/gg/mongoid/129cezk75h/no-of-connections-to-mongo-not-closing
+  #
+  def disconnect
+    Mongoid.default_session.disconnect
+  rescue => e
+    p e.message
+    Rails.logger.error e.message
+    Rails.logger.error e.stacktrace.join "\n"
+  end
+
 end
