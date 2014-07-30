4fe6bfaf604df35539b1553c33c9a2df1331e70b
reiz <robert.reiz.81@gmail.com>
2014-07-31 00:21:05 +0200

    New assets

--- public/assets/scm_app/views/loading_view.js
+++
@@ -1,1 +0,0 @@
-define(["underscore","backbone"],function(e,t){e.templateSettings={interpolate:/\{\{\=(.+?)\}\}/g,evaluate:/\{\{(.+?)\}\}/g};var n=t.View.extend({el:"#github-repos",template:e.template($("#github-loading-template").html()),render:function(){$(this.el).html(this.template({classes:"alert alert-info",message:"Please wait, we are importing data from SCM."}))}});return n});
