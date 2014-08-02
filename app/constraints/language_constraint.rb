4e961bc332bd10c9f75458bfce833f31059c00e1
reiz <robert.reiz.81@gmail.com>
2014-08-02 13:40:38 +0200

    Update versioneye-core

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -3,7 +3,7 @@ class LanguageConstraint
   def matches?(request)
     path = request.path
     path = path[1..path.length]
-    languages = distinct_languages
+    languages = LanguageService.distinct_languages
     languages << 'nodejs'
     languages.each do |lang|
       return true if /\A#{lang}\z/i =~ path || /\A#{lang}\/.*\z/i =~ path
@@ -11,31 +11,5 @@ class LanguageConstraint
     end
     false
   end
-
-  private
-
-    def distinct_languages
-      key = "distinct_languages"
-      languages = cached_languages key
-      if languages.to_s.empty?
-        languages = Product.all.distinct(:language)
-        save_in_cache key, languages
-      end
-      languages
-    end
-
-    def cached_languages key
-      Rails.cache.read key
-    rescue => e
-      Rails.logger.error e.message
-      nil
-    end
-
-    def save_in_cache key, languages
-      Rails.cache.write( key, languages, timeToLive: 1.hour )
-    rescue => e
-      Rails.logger.error e.message
-      nil
-    end
 
 end
