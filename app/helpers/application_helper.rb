6b5e573777329cede8bd23cc35b18e169b944c30
reiz <robert.reiz.81@gmail.com>
2014-10-05 16:49:48 +0200

    Fixed #561. Only show login buttons which work!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,32 @@ module ApplicationHelper
     content_for(:page_header){ page_header }
   end
 
+  def github_enabled?
+    !Settings.instance.github_base_url.to_s.empty? &&
+    !Settings.instance.github_api_url.to_s.empty? &&
+    !Settings.instance.github_client_id.to_s.empty? &&
+    !Settings.instance.github_client_secret.to_s.empty?
+  rescue => e
+    false
+  end
+
+  def bitbucket_enabled?
+    !Settings.instance.bitbucket_token.to_s.empty? &&
+    !Settings.instance.bitbucket_secret.to_s.empty?
+  rescue => e
+    false
+  end
+
+  def stash_enabled?
+    !Settings.instance.stash_base_url.to_s.empty? &&
+    !Settings.instance.stash_consumer_key.to_s.empty? &&
+    !Settings.instance.stash_private_rsa.to_s.empty?
+  rescue => e
+    false
+  end
+
+  def social_logins_enabled?
+    github_enabled? || bitbucket_enabled? || stash_enabled?
+  end
+
 end
