b8a0a88b8c8b1e900b58639e0520f2db5663d089
reiz <robert.reiz.81@gmail.com>
2014-10-07 16:46:44 +0200

    Update core

--- app/controllers/docker_controller.rb
+++ app/controllers/docker_controller.rb
@@ -17,7 +17,9 @@ class DockerController < ApplicationController
     def remote_images_hash
       images = {}
 
-      images['veye/rails_app:2.6.14'] = {
+      version = DockerImage.version("veye/rails_app")
+      version = '2.6.14' if version.to_s.empty?
+      images["veye/rails_app:#{version}"] = {
         'container_start_opts' => {
           'PortBindings' => { '8080/tcp' => [{'HostPort' => '8080'}]},
           'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm'],
@@ -27,24 +29,32 @@ class DockerController < ApplicationController
         'comments' => 'With License Whitelist'
       }
 
-      images['veye/rails_api:2.1.12'] = {
+      version = DockerImage.version("veye/rails_api")
+      version = '2.1.12' if version.to_s.empty?
+      images["veye/rails_api:#{version}"] = {
         'container_start_opts' => {
           'PortBindings' => { '9090/tcp' => [{'HostPort' => '9090'}]},
-          'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm']
+          'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm'],
+          'Binds' => ['/mnt/logs:/rails/log']
         },
         'auth' => true,
         'comments' => 'With updated versioneye-core'
       }
 
-      images['veye/tasks:1.7.4'] = {
+      version = DockerImage.version("veye/tasks")
+      version = '1.7.4' if version.to_s.empty?
+      images["veye/tasks:#{version}"] = {
         'container_start_opts' => {
-          'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm']
+          'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm'],
+          'Binds' => ['/mnt/tasks/logs:/versioneye-tasks/log']
         },
         'auth' => true,
         'comments' => 'With new scheduler'
       }
 
-      images['veye/crawlr:1.1.1'] = {
+      version = DockerImage.version("veye/crawlr")
+      version = '1.1.1' if version.to_s.empty?
+      images["veye/crawlr:#{version}"] = {
         'container_start_opts' => {
           'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm'],
           'Binds' => ['/mnt/cocoapods:/mnt/cocoapods']
@@ -53,7 +63,9 @@ class DockerController < ApplicationController
         'comments' => 'Improvements for CocoaPods'
       }
 
-      images['veye/crawlj:1.0.0'] = {
+      version = DockerImage.version("veye/crawlj")
+      version = '1.0.0' if version.to_s.empty?
+      images["veye/crawlj:#{version}"] = {
         'container_start_opts' => {
           'Links' => ['mongodb:db', 'elasticsearch:es', 'memcached:mc', 'rabbitmq:rm']
         },
@@ -62,7 +74,9 @@ class DockerController < ApplicationController
       }
 
 
-      images['reiz/mongodb:1.0.2'] = {
+      version = DockerImage.version("veye/mongodb")
+      version = '1.0.2' if version.to_s.empty?
+      images["reiz/mongodb:#{version}"] = {
         'container_start_opts' => {
           'PortBindings' => { '27017/tcp' => [{'HostPort' => '27017'}]},
           'Binds' => ['/mnt/mongodb:/data']
@@ -71,7 +85,9 @@ class DockerController < ApplicationController
         'comments' => 'With MongoDB 2.6.3'
       }
 
-      images['reiz/elasticsearch:0.9.0'] = {
+      version = DockerImage.version("veye/elasticsearch")
+      version = '0.9.0' if version.to_s.empty?
+      images["reiz/elasticsearch:#{version}"] = {
         'container_start_opts' => {
           'PortBindings' => { '9200/tcp'  => [{'HostPort' => '9200'}], '9300' => [{'HostPort' => '9300'}]},
           'Binds' => ['/mnt/elasticsearch:/data']
@@ -80,7 +96,9 @@ class DockerController < ApplicationController
         'comments' => 'With ElasticSearch 1.0.0'
       }
 
-      images['reiz/memcached:1.0.0'] = {
+      version = DockerImage.version("veye/memcached")
+      version = '1.0.0' if version.to_s.empty?
+      images["reiz/memcached:#{version}"] = {
         'container_start_opts' => {
           'PortBindings' => { '11211/tcp' => [{'HostPort' => '11211'}]}
         },
@@ -88,7 +106,9 @@ class DockerController < ApplicationController
         'comments' => 'First version'
       }
 
-      images['reiz/rabbitmq:1.0.0'] = {
+      version = DockerImage.version("veye/rabbitmq")
+      version = '1.0.0' if version.to_s.empty?
+      images["reiz/rabbitmq:#{version}"] = {
         'container_start_opts' => {
           'PortBindings' => { '5672/tcp' => [{'HostPort' => '5672'}]}
         },
