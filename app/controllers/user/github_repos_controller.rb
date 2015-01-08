234afce467e9f01b1d6d7f7654f5c4e11c7ccca8
reiz <robert.reiz.81@gmail.com>
2015-01-08 12:56:09 +0100

    Return error from Worker to ReactJS App.

--- app/controllers/user/github_repos_controller.rb
+++ app/controllers/user/github_repos_controller.rb
@@ -108,6 +108,8 @@ class User::GithubReposController < User::ScmReposController
         project = Project.find project_id 
         project_url = url_for(controller: 'projects', action: "show", id: project.id) 
         status = 'done'
+      elsif status && status.match(/\Aerror_/)
+        return status.gsub("error_", "")
       end
       
       {
