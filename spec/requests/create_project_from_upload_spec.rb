c4238c84345dab3ee2203a062be5a3e45d87913e
reiz <robert.reiz.81@gmail.com>
2015-06-15 11:17:12 +0200

    Adjust tests

--- spec/requests/create_project_from_upload_spec.rb
+++ spec/requests/create_project_from_upload_spec.rb
@@ -33,7 +33,7 @@ describe "Create Project from file uplaod" do
     response.body.should match("rails")
     response.body.should match("jquery-rails")
     response.body.should match("therubyracer")
-    response.body.should match("are outdated and")
+    response.body.should match("outdated")
   end
 
 end
