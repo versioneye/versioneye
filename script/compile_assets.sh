02c965148b8f449779106ec6c9bd00fd394c2b3a
reiz <robert.reiz.81@gmail.com>
2015-07-02 08:48:22 +0200

    Update Assets.

--- script/compile_assets.sh
+++ script/compile_assets.sh
@@ -12,6 +12,7 @@ mv config/mongoid_save.yml config/mongoid.yml
 
 mv src app/assets/javascripts/scm/
 
-cp app/assets/javascripts/libs/* public/assets/libs/
+cp app/assets/javascripts/libs/*  public/assets/libs/
+cp app/assets/javascripts/plots/* public/assets/plots/
 
 export RAILS_ENV=development
