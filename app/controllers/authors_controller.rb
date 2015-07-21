426b7dbc4355ca93a5c07f3ca29962f24f6386df
reiz <robert.reiz.81@gmail.com>
2015-07-21 11:44:25 +0200

    Show keywords on author detail page.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,27 @@ class AuthorsController < ApplicationController
   def show 
     name_id = params[:id]
     @author = Author.find_by :name_id => name_id
+    @products = Product.where(:id.in => @author.product_ids).desc(:used_by_count)
+    @keywords = fill_keywords @products
   end
+
+
+  private 
+
+
+      def fill_keywords products 
+        keywords = []
+        return keywords if products.nil? || products.empty? 
+        
+        products.each do |product|
+          next if product.nil? || product.tags.to_a.empty? 
+          
+          product.tags.each do |tag| 
+            keywords << tag if !keywords.include?(tag)
+          end
+        end
+        keywords
+      end
 
 
 end
