ab6fcc9fc109a3cb398ee66b89960ecd8689ecd3
reiz <robert.reiz.81@gmail.com>
2015-07-23 14:01:50 +0200

    Add another script to push to master

---
+++ script/push_to_master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git push origin develop 
+git push bitbucket develop 
+git checkout master 
+git merge develop 
+git push origin master 
+git push bitbucket master 
+git checkout develop 
