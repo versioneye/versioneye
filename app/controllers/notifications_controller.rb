947490f00097f890a69106ac3997537db66cddfd
reiz <robert.reiz.81@gmail.com>
2015-08-19 16:42:57 +0200

    Add auth filter to notifications controller.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,9 @@
-class NotificationsController < ApplicationController 
+class NotificationsController < ApplicationController
 
-  def index 
+  before_filter :authenticate
+
+  def index
     @notifications = Notification.by_user( current_user ).desc( :created_at ).limit(30)
-  end 
+  end
 
-end 
+end
