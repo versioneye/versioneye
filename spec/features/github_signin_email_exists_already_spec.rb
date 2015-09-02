1f4a55e7fb6be7c64de417959b78197ddb3beacf
reiz <robert.reiz.81@gmail.com>
2015-09-02 23:34:41 +0200

    Adjust GitHub login & tests.

--- spec/features/github_signin_email_exists_already_spec.rb
+++ spec/features/github_signin_email_exists_already_spec.rb
@@ -21,20 +21,15 @@ describe "Signin with GitHub" do
 
   it 'signs in a new user with github, email is already taken', js: true do
     User.all.count.should eql(0)
-    default_user.email = "test1@versioneye.com"
+    default_user.email = "test@versioneye.com"
     default_user.save
     User.all.count.should eql(1)
-    ue = UserEmail.new(:user_id => default_user.ids, :email => "test@versioneye.com")
-    expect( ue.save ).to be_truthy
 
     visit signin_path
     page.has_css? 'button.btn-github'
     within("#sm_list") do
       click_button "Login with GitHub"
     end
-
-    # find("#sm_github").click
-    # click_button "Login with GitHub"
 
     # GitHub Login Form
     fill_in "Username or Email", :with => Settings.instance.github_user
@@ -46,30 +41,8 @@ describe "Signin with GitHub" do
       click_button "Authorize application"
     end
 
-    # 2nd page with email and terms.
-    page.should have_content("Almost done.")
-    page.should have_content("The email address is already taken")
-    find_field('email').value.should eq("test@versioneye.com")
-    within("form.form-horizontal") do
-      fill_in "email", :with => ""
-      page.check "terms"
-      click_button "Sign up"
-    end
-
-    page.should have_content("is mandatory")
-    within("form.form-horizontal") do
-      fill_in "email", :with => "test_2@versioneye.com"
-      click_button "Sign up"
-    end
-
-    page.should have_content("You have to accept the Conditions")
-    within("form.form-horizontal") do
-      page.check "terms"
-      click_button "Sign up"
-    end
-
     page.should have_content("GitHub Repositories")
-    User.all.count.should eql(2)
+    User.all.count.should eql(1)
 
     visit signout_path
     visit signin_path
