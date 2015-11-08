dcd85e8a3ef74756a91155bafc83f7176ea90ecf
reiz <robert.reiz.81@gmail.com>
2015-11-08 22:16:44 +0100

    Add files for chef.

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -4,7 +4,8 @@ class LanguageConstraint
     path = request.path
     path = path[1..path.length]
     languages = LanguageService.distinct_languages
-    languages << 'nodejs'
+    languages << 'nodejs' if !languages.include?('nodejs')
+    languages << 'Chef'   if !languages.include?('Chef')
     languages.each do |lang|
       return true if /\A#{lang}\z/i =~ path || /\A#{lang}\/.*\z/i =~ path
       return true if path.match(/c\+\+/i)
