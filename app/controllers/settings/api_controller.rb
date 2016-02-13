4babf0641bc5cbd533d9227f08a20d4c2577cf75
reiz <robert.reiz.81@gmail.com>
2016-02-13 13:52:55 +0100

    Speed up Api count!

--- app/controllers/settings/api_controller.rb
+++ app/controllers/settings/api_controller.rb
@@ -1,17 +1,20 @@
 class Settings::ApiController < ApplicationController
 
+  
   before_filter :authenticate
 
+  
   def index
     @user_api = Api.find_or_initialize_by(user_id: current_user.id.to_s)
     @api_calls = 0
     if @user_api.api_key.nil?
       @user_api.api_key = 'generate new value'
     else
-      @api_calls = ApiCall.where(:user_id => current_user.ids, :api_key => @user_api.api_key).count
+      @api_calls = ApiCall.where(:api_key => @user_api.api_key).count
     end
   end
 
+  
   def update
     @user_api = Api.find_or_initialize_by(user_id: current_user.id.to_s)
     @user_api.generate_api_key!
@@ -21,5 +24,6 @@ class Settings::ApiController < ApplicationController
     end
     redirect_to :back
   end
+
 
 end
