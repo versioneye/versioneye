a0b02ac1cd2d0964fbd29980bc850575138db410
reiz <robert.reiz.81@gmail.com>
2016-02-27 16:20:50 +0100

    Switch UI tests to Chrome.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,7 +6,19 @@ Capybara.app_host = 'http://localhost:3000'
 Capybara.server_host = 'localhost'
 Capybara.server_port = '3000'
 
-Capybara.register_driver :selenium_firefox_driver do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new
-  Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+# Capybara.register_driver :selenium_firefox_driver do |app|
+#   profile = Selenium::WebDriver::Firefox::Profile.new
+#   driver = Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+#   driver.manage.window.resize_to(1000, 1000)
+# end
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app,
+    browser: :chrome,
+    desired_capabilities: {
+      "chromeOptions" => {
+        "args" => %w{ window-size=1024,768 }
+      }
+    }
+  )
 end
