b5b0c40d9ae9d4b0954274ee9b92612f853a300d
reiz <robert.reiz.81@gmail.com>
2016-03-18 14:13:25 -0700

    Update script to compile assets.

--- script/compile_assets.sh
+++ script/compile_assets.sh
@@ -12,6 +12,7 @@ mv config/mongoid_save.yml config/mongoid.yml
 
 mv src app/assets/javascripts/scm/
 
+cp app/assets/javascripts/*       public/assets/
 cp app/assets/javascripts/libs/*  public/assets/libs/
 cp app/assets/javascripts/plots/* public/assets/plots/
 cp app/assets/images/*.png public/assets/
