75c6963655b479c95c4510db36ff3e1b9f650a71
reiz <robert.reiz.81@gmail.com>
2016-05-25 14:31:43 -0700

    Remove unused file

--- spec/support/api.rb
+++
@@ -1,7 +0,0 @@
-RSpec.configure do |config|
-
-  config.include RSpec::Rails::RequestExampleGroup,
-                :type => :request,
-                :example_group => { :file_path => /spec\/api\/v1/ }
-
-end
