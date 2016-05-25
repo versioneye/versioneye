04987a0b7b9bdcc03e139b81f3d3ccf704a663dc
reiz <robert.reiz.81@gmail.com>
2016-05-25 11:34:26 -0700

    trim.

--- script/render_movie.sh
+++ script/render_movie.sh
@@ -3,12 +3,12 @@
 TITLE="http://www.VersionEye.com"
 
 command -v gource >/dev/null 2>&1 || { echo >&2 "I require gource but it's not installed. Use: brew install gource . Aborting."; exit 1; }
-command -v ffmpeg >/dev/null 2>&1 || { 
+command -v ffmpeg >/dev/null 2>&1 || {
    echo >&2 "I require ffmpeg but it's not installed. Use: brew install ffmpeg  Aborting."; exit 1; }
 
 gource --title $TITLE --auto-skip-seconds 5 --seconds-per-day 0.5 \
  --viewport 640x480 --hide filenames --logo ~/workspace/versioneye/veye/app/assets/images/icon_114.png -o - | \
 ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 \
-    -preset ultrafast -crf 1 -threads 0 -bf 0 ~/versioneye.avi 
+    -preset ultrafast -crf 1 -threads 0 -bf 0 ~/versioneye.avi
 
 echo "Movie is now rendered, check your ~/Movies folder"
