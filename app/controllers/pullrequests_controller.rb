f51d4a8df9d4755ea2f3ca18f18d7443d4ed7e3a
reiz <robert.reiz.81@gmail.com>
2016-08-16 10:04:29 +0200

    Improve PR ui integration

--- app/controllers/pullrequests_controller.rb
+++ app/controllers/pullrequests_controller.rb
@@ -1,17 +1,33 @@
 class PullrequestsController < ApplicationController
 
 
+  before_filter :load_orga
+
+
   def index
-    @prs = Pullrequest.all.desc(:updated_at) # TODO add contstraint for orga
-    @organisation = Organisation.where(:name => params[:name]).first
+    @prs = []
+    if defined?(@organisation) && !@organisation.nil?
+      @prs = Pullrequest.where(:organisation_ids => @organisation.id).desc(:updated_at)
+    elsif current_user && current_user.admin == true
+      @prs = Pullrequest.all.desc(:updated_at)
+    end
   end
 
 
   def show
-    id = params[:id]
-    @organisation = Organisation.where(:name => params[:name]).first
-    @pr = Pullrequest.find id
+    @pr = Pullrequest.find params[:id]
   end
+
+
+  private
+
+
+    def load_orga
+      orga_id = cookies.signed[:orga]
+      return nil if orga_id.to_s.empty?
+
+      @organisation = Organisation.find orga_id
+    end
 
 
 end
