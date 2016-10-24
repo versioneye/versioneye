31cf87167f83946256979c97d3691ec18a68d20e
reiz <robert.reiz.81@gmail.com>
2016-10-24 15:28:50 +0200

    Ensure that Csharp is a recognized language

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -6,6 +6,7 @@ class LanguageConstraint
     languages = LanguageService.distinct_languages
     languages << 'nodejs' if !languages.include?('nodejs')
     languages << 'Chef'   if !languages.include?('Chef')
+    languages << 'CSharp' if !languages.include?('CSharp')
     languages.each do |lang|
       return true if /\A#{lang}\z/i =~ path || /\A#{lang}\/.*\z/i =~ path
       return true if path.match(/c\+\+/i)
