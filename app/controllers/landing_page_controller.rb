acc489657c5cd150c4083516ff2a73809aabead7
reiz <robert.reiz.81@gmail.com>
2016-12-20 11:58:35 +0100

    Update lp

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -4,6 +4,7 @@ class LandingPageController < ApplicationController
   def index
     @user = User.new
     @languages = Product::A_LANGS_FILTER
+    @most_followed = Product.all.desc(:followers).limit(10)
 
     if EnterpriseService.activated?
       render :layout => 'application_lp'
