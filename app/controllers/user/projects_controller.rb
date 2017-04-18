55cedf3d2c15f6f1591c16c86fbcf06cb358075a
reiz <robert.reiz.81@gmail.com>
2017-04-18 14:11:33 +0200

    Include chilren in PDF export

--- app/controllers/user/projects_controller.rb
+++ app/controllers/user/projects_controller.rb
@@ -160,7 +160,7 @@ class User::ProjectsController < ApplicationController
     project = ProjectService.find( id )
     date_string = DateTime.now.strftime("%d_%m_%Y")
     project_name = project.name.gsub("/", "-")
-    pdf = LwlPdfService.process project, true, true
+    pdf = LwlPdfService.process project, false, true
     send_data pdf, type: 'application/pdf', filename: "#{date_string}_#{project_name}.pdf"
   rescue => e
     logger.error e.message
