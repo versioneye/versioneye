6607e5a9694773205f41ba137f01eb095d26ab91
reiz <robert.reiz.81@gmail.com>
2017-04-25 23:02:54 +0200

    Support for Rust/Crates.io

--- app/constraints/language_constraint.rb
+++ app/constraints/language_constraint.rb
@@ -7,6 +7,7 @@ class LanguageConstraint
     languages << 'nodejs' if !languages.include?('nodejs')
     languages << 'Chef'   if !languages.include?('Chef')
     languages << 'CSharp' if !languages.include?('CSharp')
+    languages << 'Rust'   if !languages.include?('Rust')
     languages.each do |lang|
       return true if /\A#{lang}\z/i =~ path || /\A#{lang}\/.*\z/i =~ path
       return true if path.match(/c\+\+/i)
