105b24e027916bd980683c86b5fe8c3398f5b3f7
reiz <robert.reiz.81@gmail.com>
2017-08-14 13:57:48 +0200

    Fix login issues if orga is nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,9 +55,13 @@ class UsersController < ApplicationController
     if @user.save
       orga = get_orga_for_login( @user ) # Create organisation for new user.
       @user.send_verification_email
-      if !cookies.signed[:plan_selected].to_s.empty?
+      if orga.nil? && Rails.env.enterprise? == true
         sign_in @user
-        redirect_to cc_organisation_path(orga)
+        redirect_to settings_profile_path( @user )
+      end
+      if !cookies.signed[:plan_selected].to_s.empty? && orga
+        sign_in @user
+        redirect_to cc_organisation_path( orga )
       end
     else
       flash[:error] = "#{t(:general_error)} - #{@user.errors.full_messages.to_sentence}"
