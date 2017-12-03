75e2b210a19b94ed6e39cc4503659f45d766d0f7
reiz <robert.reiz.81@gmail.com>
2017-12-03 18:44:02 +0100

    Disable signups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,11 @@ class UsersController < ApplicationController
 
 
   def create
+    if Rails.env.production? == true
+      flash[:error] = "VersionEye will shut down soon. Signups are disabled."
+      redirect_to signup_path and return
+    end
+
     @user = create_user_form params
     if Set['1', 'on', 'true'].include? params[:user][:terms]
       @user[:terms] = true
